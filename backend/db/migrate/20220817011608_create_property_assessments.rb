7efdd4e61013440c7c67469bb72f76a1bdf38084
cheradenine <john.burkhardt@gmail.com>
2022-08-17 06:24:13 -0400

    Adds fields 17-24 to property assessments table

--- backend/db/migrate/20220817011608_create_property_assessments.rb
+++ backend/db/migrate/20220817011608_create_property_assessments.rb
@@ -1,6 +1,14 @@
 class CreatePropertyAssessments < ActiveRecord::Migration[7.0]
   def change
     create_table :property_assessments do |t|
+      t.string :mail_addressee
+      t.string :mail_address
+      t.string :mail_city
+      t.string :mail_state, :limit => 2
+      t.string :mail_zipcode # these can be 5 or zip+4 format
+      t.float :res_floor
+      t.integer :cd_floor
+      t.integer :res_units
 
       t.timestamps
     end
