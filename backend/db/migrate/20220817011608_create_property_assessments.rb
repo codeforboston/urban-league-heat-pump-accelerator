e163855137ba2878f37f70d752d1a73c6e7eb8b5
jpb93 <jeremy.burkhardt@gmail.com>
2022-08-20 10:23:11 -0400

    Add fields 25-32 to property assessments table

--- backend/db/migrate/20220817011608_create_property_assessments.rb
+++ backend/db/migrate/20220817011608_create_property_assessments.rb
@@ -1,6 +1,14 @@
 class CreatePropertyAssessments < ActiveRecord::Migration[7.0]
   def change
     create_table :property_assessments do |t|
+      t.integer :com_units
+      t.integer :rc_units
+      t.float :land_sf
+      t.float :gross_area
+      t.float :living_area
+      t.decimal :land_value
+      t.decimal :bldg_value
+      t.decimal :total_value
 
       t.timestamps
     end
