15018177a8ecdf0ccc49030e11028b23b999adc1
Bradley <bradleydavid603@gmail.com>
2022-08-23 13:01:11 -0400

    fields 1-8 bradley

--- backend/db/migrate/20220817011608_create_property_assessments.rb
+++ backend/db/migrate/20220817011608_create_property_assessments.rb
@@ -2,6 +2,15 @@ class CreatePropertyAssessments < ActiveRecord::Migration[7.0]
   def change
     create_table :property_assessments do |t|
 
+      t.integer : "pid"
+      t.integer : "cm_id"
+      t.integer : "gis_id"
+      t.string : "st_num"
+      t.string : "st_name"
+      t.integer : "unit_num"
+      t.string : "city"
+      t.integer : "zipcode"
+
       t.timestamps
     end
   end
