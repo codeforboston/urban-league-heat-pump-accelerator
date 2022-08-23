c9a99a79e5e97422a6d1ba2ced31014e49260393
AlisonAnderson <alipanderson@gmail.com>
2022-08-23 11:51:23 -0400

    add fields 9-16 to property assesment table

--- backend/db/migrate/20220817011608_create_property_assessments.rb
+++ backend/db/migrate/20220817011608_create_property_assessments.rb
@@ -3,6 +3,17 @@ class CreatePropertyAssessments < ActiveRecord::Migration[7.0]
     create_table :property_assessments do |t|
 
       t.timestamps
+
+      t.integer :bldg_seq
+      t.integer :num_bldgs
+      t.integer :luc
+      t.string :luc_desc
+      t.string :bldg_type
+      t.string :own_occ # no char datatype?
+      t.string :owner      
+
+
+
     end
   end
 end
