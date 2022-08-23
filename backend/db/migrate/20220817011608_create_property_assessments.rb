47478f44085f248d34f1d5f14f42ff3fb85bc143
Edwin Chalumeau <edwinchalumeau@outlook.com>
2022-08-23 16:51:09 -0400

    Update 20220817011608_create_property_assessments.rb
    All Strings

--- backend/db/migrate/20220817011608_create_property_assessments.rb
+++ backend/db/migrate/20220817011608_create_property_assessments.rb
@@ -2,7 +2,15 @@ class CreatePropertyAssessments < ActiveRecord::Migration[7.0]
   def change
     create_table :property_assessments do |t|
 
-      t.timestamps
+        t.timestamps
+        t.string:BDRM_COND
+        t.string:BTHRM_STYLE1
+        t.string:BTHRM_STYLE2
+        t.string:BTHRM_STYLE3
+        t.string:KITCHEN_TYPE
+        t.string:KITCHEN_STYLE1
+        t.string:KITCHEN_STYLE2
+        t.string:KITCHEN_STYLE3
     end
   end
 end
