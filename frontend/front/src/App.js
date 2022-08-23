1c3b8ec802dd9b8f4ddda4c0f9712b204dd50a9e
Zevez7 <datguy7@gmail.com>
2022-08-22 20:02:41 -0400

    material UI enabled

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={3} m={3}>
+      <Typography variant='h3'>Material UI Enabled</Typography>
+    </Box>
   );
 }
 
