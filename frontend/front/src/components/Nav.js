576357a5586ba89d204b4c00aad095749aee8c97
Zevez7 <datguy7@gmail.com>
2022-08-23 18:29:58 -0400

    added react-router v6

---
+++ frontend/front/src/components/Nav.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+
+const nav = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color='inherit' component={Link} to='/'>
+            <Typography variant='h5'>HOME</Typography>
+          </Button>
+          <Button color='inherit' component={Link} to='/about'>
+            <Typography variant='h5'>ABOUT</Typography>
+          </Button>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default nav;
--- frontend/front/src/pages/home/Home.js
+++ frontend/front/src/pages/home/Home.js
@@ -1,13 +1,37 @@
 import React from "react";
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Grid } from "@mui/material";
 import { useSelector } from "react-redux";
 
 const Home = () => {
   const { title } = useSelector((state) => state.home);
 
   return (
-    <Box p={1} m={1}>
-      <Typography variant='h2'>{title} </Typography>
+    <Box>
+      <Box p={1} m={1}>
+        <Typography variant='h2'>{title} </Typography>
+      </Box>
+      <Box p={1} m={1}>
+        <Typography variant='h4'>Material UI Enabled</Typography>
+      </Box>
+      <Box p={1} m={1}>
+        <Typography variant='h4'>ReduxToolKit Enabled</Typography>
+      </Box>
+      <Box p={1} m={1}>
+        <Typography variant='h4'>React-Router Enabled</Typography>
+      </Box>
+      <Grid
+        container
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Grid item xs={8}>
+          xs=8
+        </Grid>
+        <Grid item xs={8}>
+          xs=8
+        </Grid>
+      </Grid>
     </Box>
   );
 };
