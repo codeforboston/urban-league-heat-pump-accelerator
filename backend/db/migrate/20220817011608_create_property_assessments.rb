31600dbf7d3da2f2e19d97064a40c311c1221f90
Edwin Chalumeau <edwinchalumeau@outlook.com>
2022-08-23 20:48:24 -0400

    Update 20220817011608_create_property_assessments.rb
    Lower case
    2 spaces

--- backend/db/migrate/20220817011608_create_property_assessments.rb
+++ backend/db/migrate/20220817011608_create_property_assessments.rb
@@ -2,15 +2,15 @@ class CreatePropertyAssessments < ActiveRecord::Migration[7.0]
   def change
     create_table :property_assessments do |t|
 
-        t.timestamps
-        t.string:BDRM_COND
-        t.string:BTHRM_STYLE1
-        t.string:BTHRM_STYLE2
-        t.string:BTHRM_STYLE3
-        t.string:KITCHEN_TYPE
-        t.string:KITCHEN_STYLE1
-        t.string:KITCHEN_STYLE2
-        t.string:KITCHEN_STYLE3
+    t.timestamps
+    t.string:bdrm_cond
+    t.string:bthrm_style1
+    t.string:bthrm_style2
+    t.string:bthrm_style3
+    t.string:kitchen_type
+    t.string:kitchen_style1
+    t.string:kitchen_style2
+    t.string:kitchen_style3
     end
   end
 end
