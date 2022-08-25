c31a9ce53150414095d154b1cb26f5364e2009fa
Zack Ganger <zganger@bu.edu>
2022-08-24 21:29:11 -0400

    Rubocop and database configurations and automatic fixes

--- backend/db/migrate/20220817011608_create_property_assessments.rb
+++ backend/db/migrate/20220817011608_create_property_assessments.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreatePropertyAssessments < ActiveRecord::Migration[7.0]
   def change
     create_table :property_assessments do |t|
-      t.timestamps 
+      t.timestamps
       t.string :heat_type
       t.string :heat_fuel
       t.string :ac_type
@@ -35,16 +37,16 @@ class CreatePropertyAssessments < ActiveRecord::Migration[7.0]
       t.string :bldg_type
       t.string :own_occ # no char datatype?
 
-      t.string :owner      
+      t.string :owner
       t.string :mail_addressee
       t.string :mail_address
       t.string :mail_city
-      t.string :mail_state, :limit => 2
+      t.string :mail_state, limit: 2
       t.string :mail_zipcode # these can be 5 or zip+4 format
       t.float :res_floor
       t.integer :cd_floor
       t.integer :res_units
-      
+
       t.string :bdrm_cond
       t.string :bthrm_style1
       t.string :bthrm_style2
