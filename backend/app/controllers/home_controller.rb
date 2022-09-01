0dc4d8c9d6fa16d42563c436b83038eca9913430
cheradenine <john.burkhardt@gmail.com>
2022-09-01 04:54:31 -0400

    Initial devise install and code gem

---
+++ backend/app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
+
+  def index
+    if current_user
+      # redirect_to go to app page?
+    else
+      redirect_to new_user_session_path
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
