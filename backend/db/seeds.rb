bd8e129267acd47eb2463668c482a5e2416e773a
Jonathan Hearn <jonnycodesalot@gmail.com>
2022-09-06 20:33:46 -0400

    add import code

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'smarter_csv'
+
+options = { verbose: true, downcase_header: true }
+# key_mapping: { id: :cm_id, sam: :st_num, PID: :pid },
+
+SmarterCSV.process('tmp/fy2022pa-4.csv', options) do |chunk|
+  chunk.each do |data_hash|
+    PropertyAssessment.create!(data_hash)
+  end
+end
