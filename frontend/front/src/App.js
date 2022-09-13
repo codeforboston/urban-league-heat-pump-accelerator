df6be7a193e20262ff8017a153d07a17a36312f0
Tino <anthony1@gmail.com>
2022-09-12 20:47:55 -0500

    login

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -13,9 +13,9 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </Box>
--- frontend/front/src/pages/login/Login.js
+++ frontend/front/src/pages/login/Login.js
@@ -3,6 +3,10 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import { Grid, Paper, Avatar, TextField, Checkbox } from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { useForm } from "react-hook-form";
 
 const style = {
   position: "absolute",
@@ -17,6 +21,33 @@ const style = {
 };
 
 const Login = (props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // Login Styless
+  const paperStyle = {
+    padding: 40,
+
+    display: "flex",
+    flexDirection: "column",
+    width: 280,
+    margin: "20px auto",
+  };
+
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnstyle = { margin: "8px 0" };
+
+  const errorStyles = {
+    color: "rgb(239 68 68 / 1)",
+    fontSize: "0.875rem",
+    lineHeight: "1.25rem",
+  };
+
+  async function LoginForms(values) {}
+
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOpen = () => setIsOpen(true);
@@ -24,31 +55,95 @@ const Login = (props) => {
 
   return (
     <div>
-      <Button color='inherit' onClick={handleOpen}>
-        <Typography variant='h5'>LOGIN</Typography>
+      <Button color="inherit" onClick={handleOpen}>
+        <Typography variant="h5">LOGIN</Typography>
       </Button>
       <Modal
         open={isOpen}
         onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
           <Box>
             <Box>
-              <Typography variant='h1'>Login23</Typography>
+              <Grid>
+                <Paper elevation={0} style={paperStyle}>
+                  <Grid align="center">
+                    <Avatar style={avatarStyle}>
+                      <LockOutlinedIcon />
+                    </Avatar>
+                    <h2>Login</h2>
+                  </Grid>
+                  <form onSubmit={handleSubmit(LoginForms)}>
+                    <TextField
+                      id="standard-basic"
+                      placeholder="Enter UserName"
+                      type="text"
+                      style={btnstyle}
+                      name="userName"
+                      fullWidth
+                      label="UserName"
+                      variant="standard"
+                      {...register("userName", {
+                        required: {
+                          value: true,
+                          message: "Please Enter UserName",
+                        },
+                      })}
+                    />
+                    <span style={errorStyles}>{errors?.userName?.message}</span>
+                    <TextField
+                      id="standard-basic"
+                      placeholder="Enter password"
+                      type="password"
+                      style={btnstyle}
+                      name="password"
+                      fullWidth
+                      label="Password"
+                      variant="standard"
+                      {...register("password", {
+                        required: {
+                          value: true,
+                          message: "Please Enter Password",
+                        },
+                      })}
+                    />
+                    <span style={errorStyles}>{errors?.password?.message}</span>
+                    <div style={{ marginTop: "5px" }}>
+                      <FormControlLabel
+                        control={<Checkbox name="checkedB" color="primary" />}
+                        label="Remember me"
+                      />
+                    </div>
+                    <Button
+                      type="submit"
+                      color="primary"
+                      variant="contained"
+                      style={{
+                        btnstyle,
+                        borderRadius: "20px",
+                        marginTop: "20px",
+                      }}
+                      fullWidth
+                    >
+                      Login in
+                    </Button>
+                  </form>
+                </Paper>
+              </Grid>
             </Box>
 
             <Box
               p={3}
-              display='flex'
-              justifyContent='space-evenly'
-              alignItems='center'
+              display="flex"
+              justifyContent="space-evenly"
+              alignItems="center"
             >
-              <Button variant='contained' onClick={handleClose}>
+              <Button variant="contained" onClick={handleClose}>
                 YES
               </Button>
-              <Button variant='contained' onClick={handleClose}>
+              <Button variant="contained" onClick={handleClose}>
                 NO
               </Button>
             </Box>
