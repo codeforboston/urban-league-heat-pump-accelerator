899a5a5130c79154c440ff35785db6b37d71f95d
cheradenine <john.burkhardt@gmail.com>
2022-09-13 20:46:58 -0400

    Implements minimal login and logout flows

--- backend/app/controllers/home_controller.rb
+++ backend/app/controllers/home_controller.rb
@@ -5,10 +5,6 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    if current_user
-      # redirect_to go to app page?
-    else
-      redirect_to new_user_session_path
-    end
+    redirect_to new_user_session_path unless current_user
   end
 end
