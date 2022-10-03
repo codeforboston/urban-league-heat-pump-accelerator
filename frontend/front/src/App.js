22bbbb39e4f285269aa7949acca6b335c7fed552
Zevez7 <datguy7@gmail.com>
2022-10-03 02:12:43 -0400

    create surveyorcontainer for routes for surveys.

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -8,6 +8,7 @@ import About from "./pages/Public/about/About";
 import Login from "./pages/Surveyor/login/Login";
 import DevContainer from "./pages/Developer/DevContainer";
 import PublicContainer from "./pages/Public/PublicContainer";
+import SurveyorContainer from "./pages/Surveyor/SurveyorContainer";
 
 function App() {
   return (
@@ -16,10 +17,8 @@ function App() {
         <Routes>
           <Route index element={<ViewMenu />} />
 
-          <Route path='surveyor' element={<Login />}>
-            <Route path='dashboard' />
-            <Route path='houseProfile' />
-          </Route>
+          <Route path='surveyor' element={<SurveyorContainer />} />
+
           <Route path='public/*' element={<PublicContainer />} />
 
           <Route path='dev/*' element={<DevContainer />} />
---
+++ frontend/front/src/pages/Surveyor/nav/Nav.js
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const Nav = () => {
+  return <Box>Nav</Box>;
+};
+
+export default Nav;
