5e993b6c11c33daa556ad3625aa079c3abe4259c
aTegart <27782045+aTegart@users.noreply.github.com>
2022-10-11 09:48:57 -0400

    account page

--- frontend/front/src/pages/Surveyor/nav/Nav.js
+++ frontend/front/src/pages/Surveyor/nav/Nav.js
@@ -1,16 +1,27 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 import React from "react";
 import LeftDrawer from "./LeftDrawer";
 const Nav = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
+      <AppBar position="static">
         <Toolbar>
           <LeftDrawer />
 
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             DASHBOARD
           </Typography>
+          <Button color="inherit" component={Link} to="account">
+            <Typography variant="h6">ACCOUNT</Typography>
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
