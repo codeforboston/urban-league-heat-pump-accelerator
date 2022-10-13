335f8a916ff27693f698093095a906e8504983bf
thiagobadini <thiagobardini@hotmail.com>
2022-10-12 20:27:43 -0400

    Created a folder for the admin view with navigation, a hamburger menu, and with two tabs (Dashboard and House)

--- frontend/front/src/pages/viewMenu/AdminView.js
+++ frontend/front/src/pages/viewMenu/AdminView.js
@@ -1,21 +1,21 @@
-import { Box, Button, Typography } from "@mui/material";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Box, Button, Typography } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const AdminView = (props) => {
   return (
     <Box my={3}>
       <Button
-        variant={"contained"}
+        variant={'contained'}
         sx={{ width: 350 }}
-        disabled
+        // disabled
         component={Link}
         to={props.link}
       >
         <Typography variant='h4'>Admin View</Typography>
       </Button>
     </Box>
-  );
-};
+  )
+}
 
-export default AdminView;
+export default AdminView
