53973e40174e28b60fecaff0eb6f7024ec86203a
jackberrypassionfruit <pashayanjack@gmail.com>
2022-10-24 22:10:06 -0400

    added buttons for edit, delete, change passwd, but not logic

--- frontend/front/src/pages/Admin/userProfile/UserProfile.js
+++ frontend/front/src/pages/Admin/userProfile/UserProfile.js
@@ -1,23 +1,30 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import React from 'react'
 import { useParams } from "react-router-dom";
 import UserData from "../../../dummyData/userTable.json";
 
 const UserProfile = () => {
   const { uid } = useParams();
-  const user = UserData.filter(user => user.uid == uid)[0];
+  const user = UserData.filter(user => user.uid === Number(uid))[0];
 
   return (
-    <Box m={3}>
-      <Typography variant='h2'>User Profile Page</Typography>
-      <Typography variant='h3'>House ID: {uid}</Typography>
-      <Typography variant='h2'>First Name: {user.firstName}</Typography>
-      <Typography variant='h2'>Last Name: {user.lastName}</Typography>
-      <Typography variant='h2'>Address: {user.address}</Typography>
-      <Typography variant='h2'>City: {user.city}</Typography>
-      <Typography variant='h2'>Zipcode: {user.zipcode}</Typography>
-      <Typography variant='h2'>Account Type: {user.accountType}</Typography>
-    </Box>
+    <>
+      <Box m={3}>
+        <Typography variant='h2'>User Profile Page</Typography>
+        <Typography variant='h3'>House ID: {uid}</Typography>
+        <Typography variant='h2'>First Name: {user.firstName}</Typography>
+        <Typography variant='h2'>Last Name: {user.lastName}</Typography>
+        <Typography variant='h2'>Address: {user.address}</Typography>
+        <Typography variant='h2'>City: {user.city}</Typography>
+        <Typography variant='h2'>Zipcode: {user.zipcode}</Typography>
+        <Typography variant='h2'>Account Type: {user.accountType}</Typography>
+      </Box>
+      <Box>
+        <Button>Edit Profile</Button>
+        <Button>Change Password</Button>
+        <Button>Delete Profile</Button>
+      </Box>
+    </>
   )
 }
 
