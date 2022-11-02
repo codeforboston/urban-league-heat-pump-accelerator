578dad8013e5cc543f1f3e12b0eb673a852fc291
Matthew Zagaja <mzagaja@users.noreply.github.com>
2022-11-02 02:08:26 +0000

    Enable Working GitHub Codespaces

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.hosts << ".preview.app.github.dev"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
