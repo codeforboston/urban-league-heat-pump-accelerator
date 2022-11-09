3572a66e6970254b5dd9ec151994c6642d818ddf
John Burkhardt <john.burkhardt@gmail.com>
2022-11-08 20:24:20 -0500

    Initial devise install and code gem (#44)
    * Initial devise install and code gem
    
    * Rubocop fixes
    
    * Add :authenticate_user!
    
    Add before_action in order to resolve error being thrown when we try to sign-up.
    
    * Implements minimal login and logout flows
    
    * reorder gems
    
    * disable auth to fix test
    
    * remove reference to undefined authenticate_user
    
    * Ignore examples.txt
    
    We don't want to check this in, it's properietary to RSpec.
    
    Co-authored-by: Matthew Zagaja <mzagaja@alum.wpi.edu>
    Co-authored-by: Matthew Zagaja <mzagaja@users.noreply.github.com>
    Co-authored-by: Anna Westland-Tegart <27782045+aTegart@users.noreply.github.com>

---
+++ backend/app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Implement controller for the home route.
+class HomeController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    redirect_to new_user_session_path unless current_user
+  end
+end
--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -71,6 +71,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.seed_data_path = 'lib/seeds/test_seeds.csv'
 end
