8cd8025f608c6f6439b0277670ec9ed6b38030fe
Casey DuLong <caseydulong@gmail.com>
2022-11-21 15:32:40 -0500

    Modifies User Profile to be like Create User Profile - Uses same form layout as Create User Profile. - Adds Change Password, Edit, and Delete buttons, based on wireframe. - Edit button toggles ability to edit form fields.

--- frontend/front/src/pages/Admin/userProfile/UserProfile.js
+++ frontend/front/src/pages/Admin/userProfile/UserProfile.js
@@ -1,14 +1,88 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 
 const UserProfile = () => {
   const { uid } = useParams();
+  const [isDisabled, setIsDisabled] = useState(true);
 
   return (
-    <Box m={3}>
-      <Typography variant='h2'>User Profile Page</Typography>
-      <Typography variant='h3'>USER ID: {uid}</Typography>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Box width={500} mt={5}>
+        <Box sx={{ bgcolor: "primary.main", color: "white" }} p={1}>
+          <Typography variant="h5">User Profile</Typography>
+        </Box>
+        <TextField
+          disabled={isDisabled}
+          label="First Name"
+          variant="standard"
+          sx={{ width: "95%", mx: 2, mt: 3 }}
+        />
+        <TextField
+          disabled={isDisabled}
+          label="Last Name"
+          variant="standard"
+          sx={{ width: "95%", mx: 2, mt: 3 }}
+        />
+        <TextField
+          disabled={isDisabled}
+          label="Email"
+          variant="standard"
+          sx={{ width: "95%", mx: 2, mt: 3 }}
+        />
+        <TextField
+          disabled={isDisabled}
+          label="Phone"
+          variant="standard"
+          sx={{ width: "95%", mx: 2, mt: 3 }}
+        />
+        <TextField
+          disabled={isDisabled}
+          label="Street Address"
+          variant="standard"
+          sx={{ width: "95%", mx: 2, mt: 3 }}
+        />{" "}
+        <TextField
+          disabled={isDisabled}
+          label="City"
+          variant="standard"
+          sx={{ width: "95%", mx: 2, mt: 3 }}
+        />{" "}
+        <TextField
+          disabled={isDisabled}
+          label="Zip Code"
+          variant="standard"
+          sx={{ width: "95%", mx: 2, mt: 3 }}
+        />{" "}
+        <TextField
+          disabled={isDisabled}
+          label="State"
+          variant="standard"
+          sx={{ width: "95%", mx: 2, mt: 3 }}
+        />
+
+        {/* BUTTONS */}
+        <Box sx={{ "display": "flex", "justify-content": "space-between" }}>
+          <Box pt={5} textAlign="left">
+            <Button variant="contained" sx={{ ml: 2 }}>
+              CHANGE PASSWORD
+            </Button>
+          </Box>
+          <Box pt={5} textAlign="right">
+            <Button variant="outlined" sx={{ ml: 2 }} onClick={() => setIsDisabled(!isDisabled)}>
+              EDIT
+            </Button>
+            <Button variant="outlined" sx={{ ml: 2 }} color="error">
+              DELETE
+            </Button>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
