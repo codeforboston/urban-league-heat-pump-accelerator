f754ac1310f1fe99fdc3dd09b5f0514e3a183eee
Zevez7 <datguy7@gmail.com>
2022-11-28 00:27:15 -0500

    confirm modal

--- frontend/front/src/pages/Developer/home/Home.js
+++ frontend/front/src/pages/Developer/home/Home.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
-import { Typography, Box, Container } from "@mui/material";
+import { Typography, Box, Container, Button, Paper } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import Heatpump from "./Heatpump";
 import RtkTesting from "../rtkDat/RtkTesting";
+import { Link } from "react-router-dom";
 
 // import { fetchHomeData } from "../../../features/home/homeSlice";
 
@@ -30,7 +31,35 @@ const Home = () => {
         >
           <Typography variant="h2">{title}</Typography>
         </Box>
-        <RtkTesting />
+
+        <Box display="flex">
+          <Box m={3}>
+            <Button color="inherit" component={Link} to="confirmation">
+              <Box
+                component={Paper}
+                width={200}
+                height={100}
+                p={1}
+                sx={{ bgcolor: "AliceBlue " }}
+              >
+                <Typography>confirmation Modal</Typography>
+              </Box>
+            </Button>
+          </Box>
+          <Box m={3}>
+            <Button color="inherit" component={Link} to="rtkq">
+              <Box
+                component={Paper}
+                width={200}
+                height={100}
+                p={1}
+                sx={{ bgcolor: "AliceBlue " }}
+              >
+                <Typography>React Tool Kit Querey Example</Typography>
+              </Box>
+            </Button>
+          </Box>
+        </Box>
       </Box>
     </Container>
   );
