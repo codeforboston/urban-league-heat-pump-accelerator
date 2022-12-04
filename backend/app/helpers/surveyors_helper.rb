4651e2e52d6deca7aee9e98257b69aa78750fd4a
Anna Westland-Tegart <27782045+aTegart@users.noreply.github.com>
2022-12-04 23:31:25 +0000

    add documentation comments

--- backend/app/helpers/surveyors_helper.rb
+++ backend/app/helpers/surveyors_helper.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
+# SurveyorsHelper is currently empty
 module SurveyorsHelper
 end
--- backend/db/migrate/20221129074717_create_surveyors.rb
+++ backend/db/migrate/20221129074717_create_surveyors.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Create the Surveyors table
 class CreateSurveyors < ActiveRecord::Migration[7.0]
   def change
     create_table :surveyors do |t|
