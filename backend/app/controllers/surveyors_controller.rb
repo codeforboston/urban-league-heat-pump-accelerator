261cda40bc05d20d78cea9ff52cf937b6d83e186
Anna Westland-Tegart <27782045+aTegart@users.noreply.github.com>
2022-12-04 23:27:07 +0000

    shorten comment length

--- backend/app/controllers/surveyors_controller.rb
+++ backend/app/controllers/surveyors_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Surveyors manages information about users of the surveying app, such as those who go door-to-door completing surveys on behalf of homeowners
+# Surveyors are users of the surveying app, such as those who go door-to-door
 class SurveyorsController < ApplicationController
   before_action :set_surveyor, only: %i[show edit update destroy]
 
