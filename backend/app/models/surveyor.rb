87f362880a26d613993147fe246cc028c36ec1dc
Anna Westland-Tegart <27782045+aTegart@users.noreply.github.com>
2022-12-05 02:32:11 +0000

    make all e all surveyor frequired for now

--- backend/app/models/surveyor.rb
+++ backend/app/models/surveyor.rb
@@ -2,4 +2,16 @@
 
 class Surveyor < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :firstname
+  validates_presence_of :lastname
+  validates_presence_of :email
+  validates_presence_of :phone
+  validates_presence_of :street_address
+  validates_presence_of :geocode
+  validates_presence_of :city
+  validates_presence_of :zipcode
+  validates_presence_of :state
+  validates_presence_of :role
+  validates_presence_of :status
 end
