25fd27b21fefe9d874f5bd8d106e07b38998e285
thiagobadini <thiagobardini@hotmail.com>
2022-12-08 11:12:16 -0500

    New template folders/files && clean code

--- frontend/front/src/pages/Public/PublicContainer.js
+++ frontend/front/src/pages/Public/PublicContainer.js
@@ -1,33 +1,16 @@
-import { Box, Container, Typography } from '@mui/material'
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './home/Home'
-import Contact from './contact/Contact'
-import About from './about/About'
-import Nav from './nav/Nav'
-import Info from './info/Info'
-import Footer from './footer/Footer'
+import { Box } from "@mui/material";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const PublicContainer = () => {
   return (
-    <Box
-      style={{
-        display: 'flex',
-        minHeight: '100vh',
-        flexDirection: 'column'
-      }}
-    >
-      <Nav />
+    <Box>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='contact' element={<Contact />} ></Route>
-        <Route path='about' element={<About />}></Route>
-        <Route path='info' element={<Info />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
-      
-      <Footer/>
     </Box>
-  )
-}
+  );
+};
 
-export default PublicContainer
+export default PublicContainer;
