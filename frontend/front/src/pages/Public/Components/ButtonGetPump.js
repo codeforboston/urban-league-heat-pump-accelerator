19081c13a806b55405d3dab0813ebe373463e2a0
thiagobadini <thiagobardini@hotmail.com>
2023-01-14 18:37:33 -0500

    Added link to ButtonGetPump

--- frontend/front/src/pages/Public/Components/ButtonGetPump.js
+++ frontend/front/src/pages/Public/Components/ButtonGetPump.js
@@ -1,9 +1,12 @@
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const ButtonGetPumnp = () => {
   return (
     <>
-      <Button variant="contained">GET A HEAT PUMP</Button>
+      <Button component={Link} to="getstarted" variant="contained">
+        GET A HEAT PUMP
+      </Button>
     </>
   );
 };
