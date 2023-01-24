be3fa56309d71ba13a95c28a3eb2e0681c9e0f8a
thiagobadini <thiagobardini@hotmail.com>
2023-01-24 12:32:04 -0500

    Added new logic to the CardLinksSection

--- frontend/front/src/pages/Public/Pages/Home/CardLinksSection.js
+++ frontend/front/src/pages/Public/Pages/Home/CardLinksSection.js
@@ -7,114 +7,101 @@ import AnimatedBoxScroll from "../../Components/AnimatedBoxScroll";
 const CardLinks = styled("div")(({ theme }) => ({
   minWidth: "350px",
   maxWidth: "480px",
-  // width: "480px",
   borderRadius: "25% 10%",
   backgroundColor: "var(--bgColor-8)",
+
+  "& .links-wrapper": {
+    "& h2": {
+      fontSize: "1.5rem",
+      fontWeight: "bold",
+      lineHeight: "1.5",
+    },
+  },
 }));
 
 const CardLinksSection = () => {
+  const linkCards = [
+    {
+      id: 1,
+      title: "Take The Survey",
+      paragraph: "Tell us your thoughts and questions about heat pumps.",
+      button: {
+        text: "Take the survey",
+        to: "survey",
+        variant: "customBtn",
+      },
+      idCSS: "survey-link-section",
+      animationCSS: "animate__heartBeat",
+    },
+    {
+      id: 2,
+      title: "Learn More About Us",
+      paragraph: "It’s our mission to bring heat pumps to Boston homes.",
+      button: {
+        text: "Learn more",
+        to: "about",
+        variant: "customBtn",
+      },
+      idCSS: "learnmore-link-section",
+      animationCSS: "animate__heartBeat",
+    },
+  ];
+
   return (
     <Grid container spacing={12} justifyContent="center">
-      <Grid
-        item
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <CardLinks
+      {linkCards.map((detail) => (
+        <Grid
+          item
+          id={detail.idCSS}
           sx={{
-            borderRadius: "25% 10%",
-            backgroundColor: "var(--bgColor-8)",
+            display: "flex",
+            flexDirection: "column",
           }}
         >
-          <AnimatedBoxScroll
-            animation="animate__heartBeat"
-            id="survey-link-section"
+          <CardLinks
+            sx={{
+              borderRadius: "25% 10%",
+              backgroundColor: "var(--bgColor-8)",
+            }}
           >
-            <Box m={4}>
-              <Typography textAlign="center" variant="h4" margimGutter>
-                Take The Survey
-              </Typography>
-              <Box
-                sx={{
-                  height: "140px",
-                  display: "flex",
-                  textAlign: "center",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Typography variant="subtitle1">
-                  Tell us your thoughts and questions about heat pumps.
+            <AnimatedBoxScroll
+              animation={detail.animationCSS}
+              id={detail.idCSS}
+            >
+              <Box id="survey-link-section" m={4}>
+                <Typography textAlign="center" variant="h5" margimGutter>
+                  {detail.title}
                 </Typography>
+                <Box
+                  sx={{
+                    height: "140px",
+                    display: "flex",
+                    textAlign: "center",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography variant="subtitle2">
+                    {detail.paragraph}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <ButtonCustom
+                    text={detail.button.text}
+                    to={detail.button.to}
+                    variant={detail.button.variant}
+                  />
+                </Box>
               </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <ButtonCustom
-                  text="Take the survey"
-                  to="survey"
-                  variant="customBtn"
-                />
-              </Box>
-            </Box>
-          </AnimatedBoxScroll>
-        </CardLinks>
-      </Grid>
-      <Grid
-        item
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <CardLinks
-          sx={{
-            borderRadius: "25% 10%",
-            backgroundColor: "var(--bgColor-8)",
-          }}
-        >
-          <AnimatedBoxScroll
-            animation="animate__heartBeat"
-            id="learnmore-link-section"
-          >
-            <Box m={4}>
-              <Typography textAlign="center" variant="h4" margimGutter>
-                Learn More About Us
-              </Typography>
-              <Box
-                sx={{
-                  height: "140px",
-                  display: "flex",
-                  textAlign: "center",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Typography variant="subtitle1">
-                  It’s our mission to bring heat pumps to Boston homes.
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <ButtonCustom
-                  text="Learn more"
-                  to="about"
-                  variant="customBtn"
-                />
-              </Box>
-            </Box>
-          </AnimatedBoxScroll>
-        </CardLinks>
-      </Grid>
+            </AnimatedBoxScroll>
+          </CardLinks>
+        </Grid>
+      ))}
     </Grid>
   );
 };
