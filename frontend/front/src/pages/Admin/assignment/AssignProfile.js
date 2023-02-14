59cd78634963945dc38237439a36c627c1dcf036
Zevez7 <datguy7@gmail.com>
2023-02-14 18:53:42 -0500

    improved assigned

--- frontend/front/src/pages/Admin/assignment/AssignProfile.js
+++ frontend/front/src/pages/Admin/assignment/AssignProfile.js
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -6,104 +6,146 @@ import { useNavigate } from "react-router-dom";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 import { DataGrid } from "@mui/x-data-grid";
 import AssignProfileTable from "../../../dummyData/assignProfileTable.json";
 
-// const columns = [
-//   { id: "hid", label: "HomeId", minWidth: 50 },
-//   { id: "address", label: "Address", minWidth: 50 },
-//   { id: "zipcode", label: "Zip Code", minWidth: 50 },
-//   { id: "city", label: "City", minWidth: 50 },
-//   { id: "surveyor", label: "Surveyor", minWidth: 50 },
-//   { id: "completed", label: "Completed", minWidth: 50 },
-// ];
-
 const rows = AssignProfileTable;
-
-const columns = [
-  { field: "id", headerName: "HomeId", width: 100 },
-  {
-    field: "surveyor",
-    headerName: "Surveyor",
-    width: 200,
-  },
-  { field: "address", headerName: "Address", width: 200 },
-  { field: "zipcode", headerName: "Zipcode", width: 120 },
-  {
-    field: "city",
-    headerName: "City",
-    width: 200,
-  },
-  {
-    field: "completed",
-    headerName: "Completed",
-    width: 200,
-  },
-
-  // {
-  //   field: "fullName",
-  //   headerName: "Full name",
-  //   description: "This column has a value getter and is not sortable.",
-  //   sortable: false,
-  //   width: 160,
-  //   valueGetter: (params) =>
-  //     `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  // },
-];
 
 const AssignProfile = () => {
   const { aid } = useParams();
   const navigate = useNavigate();
-  const [age, setAge] = React.useState("");
+  const [surveyor, setSurveyor] = React.useState("");
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setSurveyor(event.target.value);
   };
+
+  const columns = [
+    { field: "id", headerName: "HomeId", maxWidth: 100, flex: 1 },
+    {
+      field: "surveyor",
+      headerName: "Surveyor",
+      minWidth: 250,
+      flex: 1,
+      renderCell: (params) => (
+        <Button
+          variant="text"
+          color="primary"
+          size="small"
+          onClick={() => navigate(`/admin/user/userprofile/${params.row.id}`)}
+        >
+          {params.row.surveyor}
+        </Button>
+      ),
+    },
+    { field: "address", headerName: "Address", width: 200 },
+    { field: "zipcode", headerName: "Zipcode", width: 120 },
+    {
+      field: "city",
+      headerName: "City",
+      width: 200,
+      flex: 1,
+    },
+    { field: "completed", headerName: "Completed", width: 200, flex: 1 },
+    {
+      field: "survey",
+      headerName: "Survey",
+      width: 200,
+      flex: 1,
+      renderCell: (params) => (
+        <Button
+          variant="text"
+          color="primary"
+          size="small"
+          onClick={() =>
+            navigate(`/admin/survey/surveyprofile/${params.row.surveyId}`)
+          }
+        >
+          View
+        </Button>
+      ),
+    },
+    {
+      field: "hid",
+      headerName: "Home Link",
+      width: 150,
+      flex: 1,
+
+      renderCell: (params) => (
+        <Button
+          variant="text"
+          color="primary"
+          size="small"
+          onClick={() => navigate(`/admin/home/homeprofile/${params.row.id}`)}
+        >
+          View
+        </Button>
+      ),
+    },
+    {
+      field: "remove",
+      headerName: "remove",
+      width: 200,
+      flex: 1,
+      renderCell: (params) => (
+        <Button
+          variant="text"
+          color="error"
+          size="small"
+          onClick={() => console.log("remove clicked")}
+        >
+          Remove
+        </Button>
+      ),
+    },
+  ];
 
   return (
     <Box>
-      <Box textAlign="center" m={5}>
-        <Typography variant="h3">Assignment Id: {aid}</Typography>
-      </Box>
-
-      <Box p={3} flexDirection="row" display="flex">
-        <Box sx={{ minWidth: 200 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Surveyor</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={age}
-              label="Surveyor"
-              onChange={handleChange}
+      <Container style={{ maxWidth: 2000 }}>
+        <Box textAlign="center" m={5}>
+          <Typography variant="h3">Assignment Id: {aid}</Typography>
+        </Box>
+        <Box p={3} flexDirection="row" display="flex">
+          <Box sx={{ minWidth: 200 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Surveyor</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={surveyor}
+                label="Surveyor"
+                onChange={handleChange}
+              >
+                <MenuItem value={"Jane"}>Jane Austin</MenuItem>
+                <MenuItem value={"Allen"}>Allen Poe</MenuItem>
+                <MenuItem value={"Clark"}>Clark Kent</MenuItem>
+                <MenuItem value={"Clark"}>No Surveyor</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          <Box>
+            <Button
+              size={"large"}
+              variant="outlined"
+              sx={{ padding: 1.6, ml: 3 }}
             >
-              <MenuItem value={"Jane"}>Jane Austin</MenuItem>
-              <MenuItem value={"Allen"}>Allen Poe</MenuItem>
-              <MenuItem value={"Clark"}>Clark Kent</MenuItem>
-            </Select>
-          </FormControl>
+              Select
+            </Button>
+          </Box>
         </Box>
-        <Box>
-          <Button
-            size={"large"}
-            variant="outlined"
-            sx={{ padding: 1.6, ml: 3 }}
-          >
-            Select
-          </Button>
-        </Box>
-      </Box>
-
-      <div style={{ height: 800, width: "100%" }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={20}
-          rowsPerPageOptions={[20]}
-          checkboxSelection
-        />
-      </div>
+        <div style={{ width: "100%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={20}
+            rowsPerPageOptions={[20]}
+            checkboxSelection
+            autoHeight
+          />
+        </div>
+      </Container>
     </Box>
   );
 };
--- frontend/front/src/pages/Admin/assignment/Assignment.js
+++ frontend/front/src/pages/Admin/assignment/Assignment.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-import { Box } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import AssignTable from "./AssignTable";
 
 const Assignment = () => {
   return (
     <Box mt={4}>
-
-      <AssignTable />
+      <Box display="flex" justifyContent="center" alignItems="center" m={3}>
+        <Typography variant="h3">ASSIGNMENT</Typography>
+      </Box>
+      <Container style={{ maxWidth: 2000 }}>
+        <AssignTable />
+      </Container>
     </Box>
   );
 };
