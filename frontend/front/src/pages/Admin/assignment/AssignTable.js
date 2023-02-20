e3d8bb9955f65698ec0ea961d0f5cd83225cfced
Zevez7 <datguy7@gmail.com>
2023-02-20 16:38:07 -0500

    worked on assignment

--- frontend/front/src/pages/Admin/assignment/AssignTable.js
+++ frontend/front/src/pages/Admin/assignment/AssignTable.js
@@ -1,34 +1,135 @@
 import * as React from "react";
 
+import {
+  Box,
+  Button,
+  Container,
+  MenuItem,
+  TextField,
+  Typography,
+} from "@mui/material";
+import ContainerTitle from "../component/ContainerTitle";
+import data from "../../../dummyData/userSelection.json";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { Link, useNavigate } from "react-router-dom";
 import RowData from "../../../dummyData/assignTable.json";
-import { useNavigate } from "react-router-dom";
-import { Box } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-
-const columns = [
-  { field: "id", headerName: "ID", maxWidth: 100, flex: 1 },
-  {
-    field: "assigned",
-    headerName: "Assigned",
-    width: 150,
-    flex: 1,
-  },
-  { field: "homeCount", headerName: "Home", width: 110, flex: 1 },
-  { field: "surveyed", headerName: "Status", width: 110, flex: 1 },
-  { field: "completed", headerName: "Completed", width: 110, flex: 1 },
-];
 
 const rows = RowData;
 
 const AssignTable = () => {
   const navigate = useNavigate();
+  const [surveyor, setSurveyor] = React.useState("");
+
+  const handleChange = (event) => {
+    setSurveyor(event.target.value);
+  };
 
   const handRowClick = (params, event) => {
     navigate(`assignProfile/${params.id}`);
   };
 
+  const [selectionModel, setSelectionModel] = React.useState([]);
+
+  const handleSelectionModelChange = (newSelection) => {
+    setSelectionModel(newSelection);
+    console.log("Selected IDs:", newSelection);
+  };
+
+  const handleAddSurveyor = () => {
+    console.log(`add ${surveyor} to this selected home id`, selectionModel);
+  };
+  const handleRemoveSurveyor = () => {
+    console.log(
+      `remove ${surveyor} from this selected home id`,
+      selectionModel
+    );
+  };
+  const handleNameClick = (event, item) => {
+    event.stopPropagation();
+
+    return navigate(`/admin/user/userprofile/${item.id}`);
+  };
+  const columns = [
+    { field: "id", headerName: "Id", maxWidth: 100, flex: 1 },
+    {
+      field: "assigned",
+      headerName: "Assigned",
+      width: 150,
+      flex: 1,
+      renderCell: (params) =>
+        Object.values(params.row.assigned).map((item) => {
+          return (
+            <Button
+              key={item.id + item.firstName}
+              onClick={(event) => handleNameClick(event, item)}
+            >
+              {`${item.firstName} ${item.lastName}`}
+            </Button>
+          );
+        }),
+    },
+    { field: "homeCount", headerName: "Home", width: 110, flex: 1 },
+    { field: "surveyed", headerName: "Status", width: 110, flex: 1 },
+    { field: "completed", headerName: "Completed", width: 110, flex: 1 },
+    {
+      field: "view",
+      headerName: "View",
+      width: 110,
+      flex: 1,
+      renderCell: (params) => (
+        <Button
+          variant="text"
+          color="primary"
+          size="small"
+          onClick={() => navigate(`assignProfile/${params.id}`)}
+        >
+          View
+        </Button>
+      ),
+    },
+  ];
+
   return (
     <Box>
+      <Box py={3} flexDirection="row" display="flex">
+        <Box sx={{ minWidth: 200 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Surveyor</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={surveyor}
+              label="Surveyor"
+              onChange={handleChange}
+            >
+              {data.map((item) => (
+                <MenuItem key={item.id} value={item.id}>
+                  {item.firstName + " " + item.lastName}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+        <Button
+          size="large"
+          sx={{ mb: 2.5, px: 3, py: 1.5, mx: 4 }}
+          variant="outlined"
+          onClick={handleAddSurveyor}
+        >
+          Add
+        </Button>
+        <Button
+          size="large"
+          sx={{ mb: 2.5, px: 3, py: 1.5, mx: 1 }}
+          variant="outlined"
+          onClick={handleRemoveSurveyor}
+        >
+          Remove
+        </Button>
+      </Box>
       <Box sx={{ height: "100%", width: "100%" }}>
         <DataGrid
           rows={rows}
@@ -37,8 +138,9 @@ const AssignTable = () => {
           rowsPerPageOptions={[20]}
           disableSelectionOnClick
           autoHeight
-          onRowClick={handRowClick}
           checkboxSelection
+          onSelectionModelChange={handleSelectionModelChange}
+          selectionModel={selectionModel}
         />
       </Box>
     </Box>
--- frontend/front/src/pages/Admin/assignment/Assignment.js
+++ frontend/front/src/pages/Admin/assignment/Assignment.js
@@ -3,62 +3,23 @@ import React from "react";
 import {
   Box,
   Button,
-  Container,
-  MenuItem,
-  TextField,
+
   Typography,
 } from "@mui/material";
 import AssignTable from "./AssignTable";
 import ContainerTitle from "../component/ContainerTitle";
-import data from "../../../dummyData/userSelection.json";
-import InputLabel from "@mui/material/InputLabel";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
+
+import { Link } from "react-router-dom";
 
 const Assignment = () => {
-  const [surveyor, setSurveyor] = React.useState("");
-
-  const handleChange = (event) => {
-    setSurveyor(event.target.value);
-  };
 
   return (
     <ContainerTitle name={"ASSIGNMENT"}>
-      <Box py={3} flexDirection="row" display="flex">
-        <Box sx={{ minWidth: 200 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Surveyor</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={surveyor}
-              label="Surveyor"
-              onChange={handleChange}
-            >
-              {data.map((item) => (
-                <MenuItem key={item.id} value={item.id}>
-                  {item.firstName + " " + item.lastName}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-        <Button
-          size="large"
-          sx={{ mb: 2.5, px: 3, py: 1.5, mx: 4 }}
-          variant="outlined"
-        >
-          Add
-        </Button>
-        <Button
-          size="large"
-          sx={{ mb: 2.5, px: 3, py: 1.5, mx: 1 }}
-          variant="outlined"
-        >
-          Remove
+      <Box my={2}>
+        <Button component={Link} to={"unassigned"} variant={"outlined"}>
+          <Typography variant="h6">Unassigned Homes: 10</Typography>
         </Button>
       </Box>
-
       <AssignTable />
     </ContainerTitle>
   );
