3b7de36f7c991c5312ed7ffa44fa6038d131a47e
Mike Lynch <me@msl.sh>
2023-02-27 03:12:52 +0000

    Add index uniqueness constraint on (survey_id, display_order)
    This constraint is implemented at the database level.

--- backend/db/migrate/20230223042608_add_display_order_to_survey_question.rb
+++ backend/db/migrate/20230223042608_add_display_order_to_survey_question.rb
@@ -3,5 +3,7 @@
 class AddDisplayOrderToSurveyQuestion < ActiveRecord::Migration[7.0]
   def change
     add_column :survey_questions, :display_order, :integer, null: false, default: 0
+
+    add_index :survey_questions, %i[survey_id display_order], unique: true
   end
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -124,6 +124,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_042608) do
     t.datetime "updated_at", null: false
     t.integer "display_order", default: 0, null: false
     t.index ["response_options"], name: "index_survey_questions_on_response_options", using: :gin
+    t.index ["survey_id", "display_order"], name: "index_survey_questions_on_survey_id_and_display_order", unique: true
     t.index ["survey_id"], name: "index_survey_questions_on_survey_id"
   end
 
