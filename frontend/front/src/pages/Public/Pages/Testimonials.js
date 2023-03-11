cc82e3abe29b8d21c57cc5bfd892a0af394f4b97
Zevez7 <datguy7@gmail.com>
2023-03-11 16:43:41 -0500

    Revert "Frontend main"

--- frontend/front/src/pages/Public/Pages/Testimonials.js
+++ frontend/front/src/pages/Public/Pages/Testimonials.js
@@ -1,11 +1,33 @@
 import React from "react";
-import Testimonial from "../../../components/TestimonalContainer/Testimonial";
+import { Box, Grid, Typography } from "@mui/material";
 
 const Testimonials = () => {
   return (
-    <div>
-      <Testimonial />
-    </div>
+    <Box
+      sx={{
+        width: "100%",
+        height: "500px",
+        backgroundSize: "cover",
+        backgroundPosition: "bottom",
+        position: "relative",
+        zIndex: "-1",
+        flexGrow: 1,
+      }}
+    >
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ height: "100%" }}
+      >
+        <Grid item display="flex" flexDirection="column" alignItems="center">
+          <Typography variant="h4" mb={3} textAlign="center">
+            Testimonials
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
