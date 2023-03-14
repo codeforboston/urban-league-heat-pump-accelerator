4c9a89d9bb4fe8b57ed7d3c6dae51745823b3ba7
Zevez7 <datguy7@gmail.com>
2023-03-14 17:06:05 -0400

    added an alert and ordering to homes in array

--- frontend/front/src/pages/Surveyor/dashboard/ListView.js
+++ frontend/front/src/pages/Surveyor/dashboard/ListView.js
@@ -1,5 +1,5 @@
-import { Box, Typography, Button, ListItemText } from "@mui/material";
-import React from "react";
+import { Box, Typography, Button, ListItemText, Alert } from "@mui/material";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
@@ -10,7 +10,7 @@ import Avatar from "@mui/material/Avatar";
 
 import data from "../../../dummyData/homeDataCluster1.json";
 import DialogMenu from "./DialogMenu";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { selectedHome } from "../../../features/surveyor/surveyorSlice";
 
 const ListView = () => {
@@ -22,13 +22,17 @@ const ListView = () => {
     navigate("/surveyor/map");
   };
 
-  const [checked, setChecked] = React.useState([]);
+  const [checked, setChecked] = useState([]);
+
+  const [alert, setAlert] = useState(false);
 
   const handleToggle = (value) => () => {
+    // get the index of the current value in checked array
     const currentIndex = checked.indexOf(value);
-    console.log(currentIndex);
+    // copy checked array into a newChecked variable
     const newChecked = [...checked];
 
+    // toggle remove or add index into the array
     if (currentIndex === -1) {
       newChecked.push(value);
     } else {
@@ -38,13 +42,43 @@ const ListView = () => {
     setChecked(newChecked);
     console.log(checked);
   };
-  // const selectedHomeValue = useSelector((state) => state.surveyor.selectedHome);
-  // console.log(selectedHomeValue);
+
+  // does a quick comparison by order
+  const compareArrayByOrder = (a, b) => {
+    if (a.ORDER < b.ORDER) {
+      return -1;
+    }
+    if (a.ORDER > b.ORDER) {
+      return 1;
+    }
+    return 0;
+  };
+
   const generateGoogleMap = () => {
     console.log("generateGoogleMap");
     console.log();
-    dispatch(selectedHome(checked));
+
+    if (checked.length === 0) {
+      setAlert(true);
+      return console.log("No data selected");
+    }
+
+    // ensure all the home elements are sorted by their order before sending it to google map
+    const sortedCheck = checked.sort(compareArrayByOrder);
+    dispatch(selectedHome(sortedCheck));
     OnclickFx();
+  };
+
+  //
+  const SelectAllIncompleted = () => {
+    const IncompletedArray = [];
+    data.forEach((item) => {
+      if (item.completed === false) {
+        IncompletedArray.push(item);
+      }
+    });
+
+    setChecked(IncompletedArray);
   };
 
   return (
@@ -53,6 +87,13 @@ const ListView = () => {
         <Button variant="contained" size="large" onClick={generateGoogleMap}>
           GENERATE MAPS
         </Button>
+      </Box>
+      <Box pb={3}>
+        {alert ? (
+          <Alert severity="error">
+            Please Select One Home To Genereate Map
+          </Alert>
+        ) : null}
       </Box>
       <Box textAlign={"right"}>
         <Button
@@ -63,8 +104,22 @@ const ListView = () => {
         >
           Select All
         </Button>
-        <Button variant="outlined" onClick={() => setChecked([])} size="small">
+        <Button
+          variant="outlined"
+          onClick={() => setChecked([])}
+          size="small"
+          sx={{ mr: 2 }}
+        >
           Deselect All
+        </Button>
+
+        <Button
+          variant="outlined"
+          onClick={SelectAllIncompleted}
+          size="small"
+          sx={{ mr: 2 }}
+        >
+          Select Incompleted
         </Button>
       </Box>
 
