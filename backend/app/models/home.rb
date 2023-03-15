3ce2b3997d5b7130295f904cb77609cf3aab5f80
Mike Lynch <me@msl.sh>
2023-03-15 16:52:14 +0000

    Validate Home model to ensure assignment and visit order set together
    This ensures that a Home either has no assignment_id or visit_order, or both
    are set – not one or the other.

--- backend/app/models/home.rb
+++ backend/app/models/home.rb
@@ -5,4 +5,7 @@ class Home < ApplicationRecord
   belongs_to :assignment, optional: true
 
   validates :visit_order, uniqueness: { scope: :assignment_id }, allow_nil: true
+
+  validates :visit_order, presence: true, if: :assignment_id
+  validates :assignment_id, presence: true, if: :visit_order
 end
