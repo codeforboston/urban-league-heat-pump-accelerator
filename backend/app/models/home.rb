86bc0ea381ad42e37594487142ddf341ac51c817
Mike Lynch <me@msl.sh>
2023-03-15 16:43:34 +0000

    Allow multiple homes with nil visit_orders

--- backend/app/models/home.rb
+++ backend/app/models/home.rb
@@ -4,5 +4,5 @@ class Home < ApplicationRecord
   has_many :survey_visits, dependent: nil
   belongs_to :assignment, optional: true
 
-  validates :visit_order, uniqueness: { scope: :assignment_id }
+  validates :visit_order, uniqueness: { scope: :assignment_id }, allow_nil: true
 end
