f35f41c2c3384f81a40c41233a767f83748fa6e5
Matthew Zagaja <mzagaja@alum.wpi.edu>
2023-03-18 17:13:42 -0400

    Appease Rubocop

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,13 +1,17 @@
-class Users::SessionsController < Devise::SessionsController
-  respond_to :json
+# frozen_string_literal: true
 
-  private
+module Users
+  class SessionsController < Devise::SessionsController
+    respond_to :json
 
-  def respond_with(resource, _opts = {})
-    render json: resource
-  end
+    private
 
-  def respond_to_on_destroy
-    head :no_content
+    def respond_with(resource, _opts = {})
+      render json: resource
+    end
+
+    def respond_to_on_destroy
+      head :no_content
+    end
   end
 end
