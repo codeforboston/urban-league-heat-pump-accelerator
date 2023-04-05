31ac39be63844c9057f7ee57102b4aac95872d95
thiagobadini <thiagobardini@hotmail.com>
2023-04-05 16:03:21 -0400

    created a description about the script/copy-db.js

--- frontend/front/scripts/copy-db.js
+++ frontend/front/scripts/copy-db.js
@@ -1,4 +1,12 @@
 // Vercel deployment: https://vercel.com/docs/serverless-functions/introduction
+// To access and visualize the data from the Vercel-hosted API, simply
+// visit the API endpoint URLs using the Vercel deployment domain
+// (e.g., https://https://bhpa.vercel.app/api/your-endpoint).
+//
+// Add a new script called "copy-db" to copy the contents of ./src/dummyData/jsonServerData/db.js to ./api/db.js.
+// Also modified the "start" script to run "copy-db" before starting the app using react-scripts.
+// This will ensure that the db.js file is copied to the right location before the app is started.
+
 const fs = require("fs");
 
 const srcPath = "./src/dummyData/jsonServerData/db.js";
