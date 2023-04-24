f9e724832b59a3489f8f7f906e41740d36dd799b
mcarre20 <marlycarre@gmail.com>
2023-04-24 16:51:21 -0400

    first update

---
+++ frontend/front/src/pages/Public/Components/AboutHeatPumpsCards.js
@@ -0,0 +1,96 @@
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Link,
+  Typography,
+  styled,
+} from "@mui/material";
+const CardTitle = styled(Typography)(({ theme }) => ({
+  [theme.breakpoints.down("lg")]: {
+    fontSize: "1.25rem",
+  },
+  [theme.breakpoints.up("lg")]: {
+    fontSize: "2.875rem",
+  },
+
+  fontWeight: 500,
+  color: "var(--color-text-8)",
+  marginTop: "10px",
+}));
+
+function AboutHeatPumpCards({
+  mediaType,
+  mediaSource,
+  title,
+  body,
+  link,
+  linkDescription,
+}) {
+  return (
+    <Card
+      sx={{
+        boxShadow: 0,
+        borderRadius: "0px",
+        display: "flex",
+        flexDirection: { xs: "column", lg: "row" },
+        alignItems: "center",
+        gap: { lg: "60px" },
+      }}
+    >
+      <Box
+        sx={{
+          height: { xs: "198px", lg: "367px" },
+          width: { lg: "654px" },
+          order: { lg: "2" },
+        }}
+      >
+        <CardMedia
+          component={mediaType}
+          height="100%"
+          width="100%"
+          src={mediaSource}
+        />
+      </Box>
+      <Box sx={{ maxWidth: { lg: "586px" }, maxHeight: "auto" }}>
+        <CardContent sx={{ padding: 0 }}>
+          <CardTitle variant="h4">{title}</CardTitle>
+          <Typography
+            sx={{
+              color: "var(--colot-trext-6)",
+              marginTop: "10px",
+              fontSize: { lg: "1.875rem" },
+            }}
+          >
+            {body}
+          </Typography>
+        </CardContent>
+        {link && (
+          <CardActions
+            sx={{
+              padding: "0px",
+              marginTop: { xs: "10px", lg: "20px" },
+              justifyContent: "end",
+            }}
+          >
+            <Link
+              href={link}
+              sx={{
+                textDecoration: "none",
+                fontSize: { xs: "0.75rem", lg: "1.875rem" },
+                fontWeight: 700,
+                color: "var(--color-text-8)",
+              }}
+            >
+              {linkDescription.toUpperCase()}
+            </Link>
+          </CardActions>
+        )}
+      </Box>
+    </Card>
+  );
+}
+
+export default AboutHeatPumpCards;
--- frontend/front/src/pages/Public/Pages/AboutHeatPump.js
+++ frontend/front/src/pages/Public/Pages/AboutHeatPump.js
@@ -1,32 +1,86 @@
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import streets from "../../../assets/images/copywritingImages/StrengthenCommunity.jpg";
+import homeExterior from "../../../assets/images/home-exterior.jpg";
+import AboutHeatPumpCards from "../Components/AboutHeatPumpsCards";
+import PublicPageTile from "../Components/PublicPageTitle";
+
+const cardContent = [
+  {
+    mediaType: "img",
+    mediaSource: homeExterior,
+    title: "What is a Heat Pump?",
+    body: "Check out this comprehensive article that Carrier, a heat pump manufacturer, has written up about what a heat pump is and how they work.",
+    link: "https://www.carrier.com/residential/en/us/products/heat-pumps/what-is-a-heat-pump-how-does-it-work/",
+    linkDescription: "read article",
+  },
+  {
+    mediaType: "iframe",
+    mediaSource: "https://www.youtube.com/embed/iQaycSD5GWE",
+    title: "How does a heat pump work?",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+  {
+    mediaType: "iframe",
+    mediaSource: "https://youtube.com/embed/PIulbHyK0bc",
+    title: "Why are heat pumps better",
+    body: "This video by Vox explains why heat pumps are better for the environment than fossil fuel heaters.",
+  },
+  {
+    mediaType: "img",
+    mediaSource: streets,
+    title: "What do other people say",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.",
+  },
+];
 
 function AboutHeatPump() {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        height: "500px",
-        backgroundSize: "cover",
-        backgroundPosition: "bottom",
-        position: "relative",
-        zIndex: "-1",
-        flexGrow: 1,
-      }}
-    >
-      <Grid
-        container
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        sx={{ height: "100%" }}
+    <Box>
+      <PublicPageTile pageTitle="About Heat Pumps" />
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: { xs: "10px", lg: "60px" },
+          maxWidth: { xs: "353px", lg: "1300px" },
+          marginTop: { xs: "20px", lg: "60px" },
+          paddingLeft: 0,
+        }}
       >
-        <Grid item display="flex" flexDirection="column" alignItems="center">
-          <Typography variant="h4" mb={3} textAlign="center">
-            About Heat Pump
-          </Typography>
-        </Grid>
-      </Grid>
+        <Typography
+          variant="h3"
+          sx={{
+            maxWidth: "905px",
+            fontSize: { xs: "1.5rem", lg: "3.875rem" },
+            fontWeight: 500,
+            color: "var(--color-text-8)",
+          }}
+        >
+          Learn how heat pumps work and why they are right for you!
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: { xs: "10px", lg: "60px" },
+          }}
+        >
+          {cardContent.map((card, index) => {
+            return (
+              <AboutHeatPumpCards
+                key={index}
+                mediaType={card.mediaType}
+                mediaSource={card.mediaSource}
+                title={card.title}
+                body={card.body}
+                link={card.link}
+                linkDescription={card.linkDescription}
+              />
+            );
+          })}
+        </Box>
+      </Container>
     </Box>
   );
 }
