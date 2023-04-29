3f97bf5fef1c24ce2e8b13d6e5131d40d67e551a
thiagobadini <thiagobardini@hotmail.com>
2023-04-28 21:38:08 -0400

    Fix CSS to conform with code standards

--- frontend/front/src/pages/Public/Pages/AboutUs/PartnerTile.js
+++ frontend/front/src/pages/Public/Pages/AboutUs/PartnerTile.js
@@ -2,34 +2,31 @@ import React from "react";
 import { Box, Stack, Typography, Link } from "@mui/material";
 
 const PartnerTile = ({ partnerName, paragraphText, image, website }) => {
-
   return (
-    <Box margin={3} container>
+    <Box my={3}>
       <Stack
         direction="row"
-        alignItems={'center'}
-        justifyContent={'center'}
-        spacing={1}
-        flexWrap={window.innerWidth < 600 ? 'wrap' : 'nowrap'}
+        alignItems={"center"}
+        justifyContent={"center"}
+        flexDirection={{ xs: "column", md: "row" }}
       >
+        <Link
+          component="img"
+          src={image}
+          href={website}
+          target="_blank"
+          rel="noopener noreferrer"
+          width={220}
+          m={2}
+        />
+
         <Box>
-          <Link
-            component="img"
-            src={image}
-            href={website}
-            target="_blank"
-            rel="noopener noreferrer"
-            width={220}
-            margin={2}
-          />
-        </Box>
-        <Box>
-          <Typography color='#0a0b0b'>
+          <Typography color="#0a0b0b">
             <b>{partnerName}</b> {paragraphText}
           </Typography>
         </Box>
-      </Stack >
-    </Box >
+      </Stack>
+    </Box>
   );
 };
 
