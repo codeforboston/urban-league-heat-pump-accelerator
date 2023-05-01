ecdf0390245164128d6f593ba1fc39b534d2ee46
Leo W <leo.westebbe@gmail.com>
2023-05-01 19:14:50 -0400

    created src/dummyData/jsonServerData/survey_visits.js with all null values, datatypes as comments

---
+++ frontend/front/src/dummyData/jsonServerData/survey_visits.js
@@ -0,0 +1,9 @@
+module.exports = [
+  {
+    user_id: null, // type: bigint, null: false
+    completed: null, // type: boolean,
+    created_at: null, // type: datetime, null: false
+    updated_at: null, // type: datetime, null: false
+    home_id: null, // type: bigint,
+  },
+];
