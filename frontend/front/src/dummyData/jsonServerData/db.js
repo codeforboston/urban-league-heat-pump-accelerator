1482eda80963cce5a715474b041cb2063339e1d7
Leo W <leo.westebbe@gmail.com>
2023-05-01 19:18:56 -0400

    add new jsonserver config files to db.js

--- frontend/front/src/dummyData/jsonServerData/db.js
+++ frontend/front/src/dummyData/jsonServerData/db.js
@@ -3,6 +3,12 @@ const homes = require("./homes.js");
 const users = require("./users.js");
 const surveys = require("./surveys.js");
 const homesurvey = require("./homesurvey.js");
+const property_assessments = require("./property_assessments.js");
+const survey_answers = require("./survey_answers.js");
+const survey_responses = require("./survey_responses.js");
+const survey_questions = require("./survey_questions.js");
+const survey_visits = require("./survey_visits.js");
+const surveyors = require("./surveyors.js");
 
 module.exports = () => ({
   assignments,
@@ -10,4 +16,10 @@ module.exports = () => ({
   users,
   surveys,
   homesurvey,
+  property_assessments,
+  survey_answers,
+  survey_responses,
+  survey_questions,
+  survey_visits,
+  surveyors,
 });
