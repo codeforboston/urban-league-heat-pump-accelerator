fe911b68a579e20fe1caec992d061316f52574af
Leo W <leo.westebbe@gmail.com>
2023-05-01 19:16:47 -0400

    created src/dummyData/jsonServerData/surveyors.js with all null values, datatypes as comments

---
+++ frontend/front/src/dummyData/jsonServerData/surveyors.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    user_id: null, // type: bigint, null: false
+    firstname: null, // type: string
+    lastname: null, // type: string
+    email: null, // type: string
+    phone: null, // type: string
+    street_address: null, // type: string
+    geocode: null, // type: string
+    city: null, // type: string
+    zipcode: null, // type: string
+    state: null, // type: string
+    role: null, // type: string
+    status: null, // type: string
+    created_at: null, // type: datetime, null: false
+    updated_at: null, // type: datetime, null: false
+  },
+];
