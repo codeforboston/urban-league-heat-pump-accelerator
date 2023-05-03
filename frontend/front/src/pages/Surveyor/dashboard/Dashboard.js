b83517e538c254eaf6ced1331ec403e7e8e988a5
Lisa Nguyen <lisanguyensc@gmail.com>
2023-05-03 11:02:36 -0400

    Removed JSON server status message

--- frontend/front/src/pages/Surveyor/dashboard/Dashboard.js
+++ frontend/front/src/pages/Surveyor/dashboard/Dashboard.js
@@ -7,39 +7,8 @@ const Dashboard = () => {
   const { data, isLoading, isSuccess, isError, error } =
     useGetSurveyorAssignmentQuery();
 
-  let content = "no data";
-
-  if (isLoading) {
-    content = <Box> Is loading</Box>;
-  } else if (isSuccess) {
-    content = <Box>Status: Is successful</Box>;
-    console.log("dashboard", data);
-    // content = data.map((item) => {
-    //   return (
-    //     <Box key={item.id} border={1}>
-    //       <HomeItem data={item} />
-    //       <Button
-    //         onClick={() =>
-    //           updateHomeData({ ...item, completed: !item.completed })
-    //         }
-    //       >
-    //         Toggle Completed
-    //       </Button>
-    //       <Button onClick={() => deleteHomeData(item)}>delete</Button>
-    //     </Box>
-    //   );
-    // });
-  } else if (isError) {
-    content = <Box>{error.error}</Box>;
-  }
-
   return (
     <Box>
-      <Box m={3} p={2}>
-        <Typography>Json Server api</Typography>
-        {content}
-      </Box>
-
       <Box display={"flex"} justifyContent="center">
         <ListView />
       </Box>
