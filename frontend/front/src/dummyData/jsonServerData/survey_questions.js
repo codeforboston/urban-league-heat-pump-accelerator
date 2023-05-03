d2396b00a7f68c297d2bd8ec469390eb2f69886a
Leo W <leo.westebbe@gmail.com>
2023-05-03 13:54:28 -0400

    add id fields to all new jsonserver config files

--- frontend/front/src/dummyData/jsonServerData/survey_questions.js
+++ frontend/front/src/dummyData/jsonServerData/survey_questions.js
@@ -1,5 +1,26 @@
 module.exports = [
   {
+    id: 1,
+    text: null, // type: text
+    response_type: null, // type: integer
+    response_options: null, // type: string, array: true
+    survey_id: null, // type: bigint, null: false
+    created_at: null, // type: datetime, null: false
+    updated_at: null, // type: datetime, null: false
+    display_order: null, // type: integer, default: 0, null: false
+  },
+  {
+    id: 2,
+    text: null, // type: text
+    response_type: null, // type: integer
+    response_options: null, // type: string, array: true
+    survey_id: null, // type: bigint, null: false
+    created_at: null, // type: datetime, null: false
+    updated_at: null, // type: datetime, null: false
+    display_order: null, // type: integer, default: 0, null: false
+  },
+  {
+    id: 3,
     text: null, // type: text
     response_type: null, // type: integer
     response_options: null, // type: string, array: true
