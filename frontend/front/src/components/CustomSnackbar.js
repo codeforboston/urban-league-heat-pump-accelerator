66b18fe8341f8270fac297d87299ee7ed424eec4
Lisa Nguyen <lisanguyensc@gmail.com>
2023-05-10 20:18:58 -0400

    Updated CustomSnack bar to be error by default

--- frontend/front/src/components/CustomSnackbar.js
+++ frontend/front/src/components/CustomSnackbar.js
@@ -1,6 +1,6 @@
 import { Snackbar, Alert } from "@mui/material";
 
-function CustomSnackbar({ open, onClose, message, severity }) {
+function CustomSnackbar({ open, onClose, message = 'This is an error Snackbar', severity = 'error' }) {
     return (
       <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
         <Alert onClose={onClose} severity={severity}>
