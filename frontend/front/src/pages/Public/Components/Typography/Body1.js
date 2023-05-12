66f0d393c1b43201fc6ab45c201925e19baa212c
thiagobardini <thiagobardini@hotmail.com>
2023-05-12 16:07:24 -0400

    Clean code

--- frontend/front/src/pages/Public/Components/Typography/Body1.js
+++
@@ -1,7 +0,0 @@
-import { Typography } from "@mui/material";
-
-function Heading1({ text }) {
-  return <Typography sx={{}}>{text}</Typography>;
-}
-
-export default Heading1;
--- frontend/front/src/pages/Public/Pages/Home/Testimonial.js
+++ frontend/front/src/pages/Public/Pages/Home/Testimonial.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 import { Typography, Avatar, Box } from "@mui/material";
-import AnimatedBox from "../../Components/AnimatedBox";
 
 const items = [
   {
