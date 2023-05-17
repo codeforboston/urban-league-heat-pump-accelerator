89f818d2c10af2e18e302cd12b0ae8c2bf7ec3f4
Mike Lynch <me@msl.sh>
2023-05-17 03:25:01 +0000

    Add accepts_nested_attributes_for on survey visit/response
    Add this attribute to survey_visit and survey_response, so that we can
    optionally create the entire survey response hierarchy at the same time as
    creating the survey_visit instance.

--- backend/app/models/survey_response.rb
+++ backend/app/models/survey_response.rb
@@ -3,4 +3,6 @@
 class SurveyResponse < ApplicationRecord
   belongs_to :survey
   belongs_to :survey_visit
+  has_many :survey_answers
+  accepts_nested_attributes_for :survey_answers
 end
--- backend/app/models/survey_visit.rb
+++ backend/app/models/survey_visit.rb
@@ -4,4 +4,5 @@ class SurveyVisit < ApplicationRecord
   belongs_to :user
   belongs_to :home
   has_one :survey_response
+  accepts_nested_attributes_for :survey_response
 end
