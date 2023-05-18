72751c3d0889fc208db6aa6ac47dd62d24300525
Leo W <leo.westebbe@gmail.com>
2023-05-18 16:38:35 -0400

    HomeTable displays all available data in the columns. Surveyor and Completed are missing

--- frontend/front/src/pages/Admin/home/HomeTable.js
+++ frontend/front/src/pages/Admin/home/HomeTable.js
@@ -2,28 +2,57 @@ import * as React from "react";
 
 import { DataGrid } from "@mui/x-data-grid";
 import { useNavigate } from "react-router-dom";
+import { useGetHomesQuery } from "../../../api/apiSlice";
+import { Box, CircularProgress } from "@mui/material";
+
+const getAddress = (params) => {
+  let unit_number = "";
+  if (params.getValue(params.id, "unit_number")) {
+    unit_number = params.getValue(params.id, "unit_number");
+  }
+  return `${params.getValue(params.id, "street_number")} ${params.getValue(
+    params.id,
+    "street_name"
+  )} ${unit_number && "Unit " + unit_number}`;
+};
 
 const columns = [
   { field: "id", headerName: "Id", width: 50 },
   { field: "surveyor", headerName: "Surveyor", width: 200 },
-  { field: "address", headerName: "Address", width: 200 },
+  {
+    field: "address",
+    valueGetter: getAddress,
+    headerName: "Address",
+    width: 200,
+  },
   { field: "city", headerName: "City", width: 200 },
-  { field: "zipcode", headerName: "Zip Code", width: 200 },
+  { field: "zip_code", headerName: "Zip Code", width: 200 },
   { field: "completed", headerName: "Completed", width: 200 },
 ];
 
-const rows = [];
-
 const HomeTable = () => {
+  const {
+    data: homesData,
+    error: homesError,
+    isLoading: isHomesDataLoading,
+  } = useGetHomesQuery();
   const navigate = useNavigate();
 
   const onRowClick = (row) => {
     navigate(`homeprofile/${row.id}`);
   };
 
+  if (isHomesDataLoading) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress />
+      </Box>
+    );
+  }
+
   return (
     <DataGrid
-      rows={rows}
+      rows={homesData}
       columns={columns}
       pageSize={20}
       rowsPerPageOptions={[20]}
