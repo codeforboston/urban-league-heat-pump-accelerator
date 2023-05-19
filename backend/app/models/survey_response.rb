6ee0797977c1925a62b7da1b9d26823b4df199d8
Mike Lynch <me@msl.sh>
2023-05-19 15:20:55 +0000

    Fix rubocop violation

--- backend/app/models/survey_response.rb
+++ backend/app/models/survey_response.rb
@@ -3,6 +3,6 @@
 class SurveyResponse < ApplicationRecord
   belongs_to :survey
   belongs_to :survey_visit
-  has_many :survey_answers
+  has_many :survey_answers, dependent: :destroy
   accepts_nested_attributes_for :survey_answers
 end
