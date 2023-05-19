88b61f09fa16bdc997c168c13f82967f87889fed
Leo W <leo.westebbe@gmail.com>
2023-05-19 10:23:59 -0400

    load homes data using rtkquery hook in HomeProfile. Render spinner if loading.

--- frontend/front/src/pages/Admin/home/HomeProfile.js
+++ frontend/front/src/pages/Admin/home/HomeProfile.js
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useGetHomeQuery } from "../../../api/apiSlice";
 
 const HomeProfile = () => {
   const { hid } = useParams();
+
+  // Load home data
+  const {
+    data: homeData,
+    isLoading: isHomeDataLoading,
+    error: homeError,
+  } = useGetHomeQuery(hid);
+
+  console.log(homeData);
+
   const [editMode, setEditMode] = useState(false);
 
   // Conditional Buttons
@@ -45,6 +62,14 @@ const HomeProfile = () => {
     );
   }
 
+  if (isHomeDataLoading) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress />
+      </Box>
+    );
+  }
+
   return (
     <Box
       display="flex"
@@ -61,60 +86,70 @@ const HomeProfile = () => {
           label="Street Number"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
+          value={homeData.street_number}
         />
         <TextField
           disabled={!editMode}
           label="Street Name"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
+          value={homeData?.street_name}
         />
         <TextField
           disabled={!editMode}
           label="City"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
+          value={homeData?.city}
         />
         <TextField
           disabled={!editMode}
           label="Zip Code"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
+          value={homeData?.zip_code}
         />
         <TextField
           disabled={!editMode}
           label="Building Type"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
-        />{" "}
+          value={homeData?.building_type}
+        />
         <TextField
           disabled={!editMode}
           label="Owner"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
-        />{" "}
+          value={homeData?.owner}
+        />
         <TextField
           disabled={!editMode}
           label="Year Built"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
-        />{" "}
+          value={homeData?.year_built}
+        />
         <TextField
           disabled={!editMode}
           label="Heat Type"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
+          value={homeData?.heat_type}
         />
         <TextField
           disabled={!editMode}
           label="Heat Fuel"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
+          value={homeData?.heat_fuel}
         />
         <TextField
           disabled={!editMode}
-          label="Surveyer ID"
+          label="Surveyor ID"
           variant="standard"
           sx={{ width: "95%", mx: 2, mt: 3 }}
+          value={homeData?.surveyor_id}
         />
         {/* BUTTONS */}
         <Box pt={5} textAlign="right">
