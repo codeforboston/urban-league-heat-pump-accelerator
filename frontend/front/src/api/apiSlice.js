0210bd6d419fcefb2e13d4bab5fc0471e6b71deb
Leo W <leo.westebbe@gmail.com>
2023-05-22 16:38:15 -0400

    comment out useGetSurveyorsByAssignmentIdQuery hook for now

--- frontend/front/src/api/apiSlice.js
+++ frontend/front/src/api/apiSlice.js
@@ -329,7 +329,7 @@ export const {
   useCreateSurveyorMutation,
   useGetSurveyorQuery,
   useGetSurveyorsQuery,
-  useGetSurveyorsByAssignmentIdQuery,
+  // useGetSurveyorsByAssignmentIdQuery,
 
   // Home
   useDeleteHomeMutation,
