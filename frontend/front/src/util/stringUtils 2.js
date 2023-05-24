a950553f5dd05f5aa4f4fad100d0f6e73aa10561
thiagobadini <thiagobardini@hotmail.com>
2023-05-23 23:13:43 -0400

    Clean code

--- frontend/front/src/util/stringUtils 2.js
+++
@@ -1,7 +0,0 @@
-// given a sentence, get the first X words, lowercased and hyphenated
-export const sentenceAsLink = (string) =>
-  string
-    .split(" ")
-    .slice(0, 10)
-    .map((s) => s.replace(/[^\w\s]/g, "").toLowerCase())
-    .join("-");
