ef94bce1b7b2fa646b85f0ce58aa388887c507e6
thiagobardini <thiagobardini@hotmail.com>
2023-05-31 15:49:04 -0400

    Added isError handling for data absence

--- frontend/front/src/pages/Admin/home/HomeTable.js
+++ frontend/front/src/pages/Admin/home/HomeTable.js
@@ -3,7 +3,7 @@ import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { useNavigate } from "react-router-dom";
 import { useGetHomesQuery } from "../../../api/apiSlice";
-import { Box, Button } from "@mui/material";
+import { Box, Button} from "@mui/material";
 import Loader from "../../../components/Loader";
 import CustomSnackbar from "../../../components/CustomSnackbar";
 
