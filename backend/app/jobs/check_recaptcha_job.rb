bc247bcc71c90f553fe70b3caeb1abc596357b95
Matthew Zagaja <mzagaja@users.noreply.github.com>
2023-06-07 00:11:41 +0000

    Load secret key from Rails Credentials
    Co-authored-by: Mike Lynch <mikelynch@users.noreply.github.com>
    Co-authored-by: Anna Westland-Tegart <aTegart@users.noreply.github.com>

--- backend/app/jobs/check_recaptcha_job.rb
+++ backend/app/jobs/check_recaptcha_job.rb
@@ -8,8 +8,7 @@ class CheckRecaptchaJob < ApplicationJob
   include CheckRecaptcha
 
   def perform(survey_response_id, response_token, recaptcha_action)
-    # TODO: load actual key from config
-    secret_key = 'abc'
+    secret_key = Rails.application.credentials['recaptcha_secret_key']
 
     score = check_recaptcha secret_key, response_token, recaptcha_action
 
