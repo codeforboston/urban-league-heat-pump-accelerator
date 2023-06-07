2ca3cde3df326e3b3234662239b7ea2e26c6c930
Matthew Zagaja <mzagaja@users.noreply.github.com>
2023-06-07 00:36:55 +0000

    Add Recaptcha Token
    * Add syntax_suggest gem for finding missing ends.
    * Add header for receiving the Recaptcha token from the front end.
    * Automatically set a no spam score for items submitted from logged in
    users.

--- backend/app/models/survey_visit.rb
+++ backend/app/models/survey_visit.rb
@@ -5,4 +5,8 @@ class SurveyVisit < ApplicationRecord
   belongs_to :home
   has_one :survey_response
   accepts_nested_attributes_for :survey_response
+
+
+
+  def check_recaptcha
 end
--- backend/lib/check_recaptcha.rb
+++ backend/lib/check_recaptcha.rb
@@ -4,8 +4,10 @@ require 'net/https'
 
 module CheckRecaptcha
   def check_recaptcha(secret_key, response_token, recaptcha_action)
-    uri = URI.parse("https://www.google.com/recaptcha/api/siteverify?secret=#{secret_key}&response=#{response_token}")
-    response = Net::HTTP.get_response(uri)
+    # Verify reCAPTCHA token per https://developers.google.com/recaptcha/docs/verify
+    uri = URI.parse('https://www.google.com/recaptcha/api/siteverify')
+
+    response = Net::HTTP.post(uri, 'secret' => secret_key, 'response' => response_token)
     json = JSON.parse(response.body)
 
     # TODO: What to do if not successful validating?
