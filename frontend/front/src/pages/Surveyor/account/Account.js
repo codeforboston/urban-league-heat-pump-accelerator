f01e6e561d2614c2d5605356b8e420d05463f272
Leo Westebbe <leo.westebbe@gmail.com>
2023-06-18 01:43:55 -0400

    *get current user account data via rtk-query *load data on page *error handling and loading

--- frontend/front/src/pages/Surveyor/account/Account.js
+++ frontend/front/src/pages/Surveyor/account/Account.js
@@ -3,42 +3,64 @@ import React from "react";
 import AccountDetail from "./AccountDetail";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../../features/login/loginSlice";
+import { useGetSurveyorQuery } from "../../../api/apiSlice";
+import Loader from "../../../components/Loader";
+import CustomSnackbar from "../../../components/CustomSnackbar";
 
 const Account = () => {
-  const { firstName, lastName, email, address, phoneNumber } = useSelector(
-    (state) => state.account
-  );
+  const { id: userId } = useSelector(selectCurrentUser);
+
+  const {
+    data: accountData,
+    isError: isAccountDataError,
+    isLoading: isAccountDataLoading,
+  } = useGetSurveyorQuery(userId);
+
+
   return (
     <Box>
-      <Grid container direction="column" rowSpacing={1}>
-        <Grid item xs={12}>
-          <Box display="flex" px={2} pt={3}>
-            <Typography variant="h2" component="h2">
-              My Account
-            </Typography>
-          </Box>
-        </Grid>
-        <AccountDetail label="First Name" value={firstName} />
-        <AccountDetail label="Last Name" value={lastName} />
-        <AccountDetail label="Email" value={email} />
-        <AccountDetail label="Address" value={address} />
-        <AccountDetail label="Phone Number" value={phoneNumber} />
-      </Grid>
-      <Grid container direction="column" rowSpacing={4}>
-        <Grid item xs={12}>
-          <Box display="flex" mt={3} mb={2} px={2}>
-            <Button variant="contained" component={Link} to="edit">
-              Edit
-            </Button>
-          </Box>
-          <Box p={1} sx={{ borderBottom: "1px dashed grey" }} />
-          <Box display="flex" mt={3} mb={2} px={2}>
-            <Button variant="contained" disabled={true}>
-              Request New Assignment
-            </Button>
-          </Box>
-        </Grid>
-      </Grid>
+      {isAccountDataLoading ? (
+        <Loader />
+      ) : isAccountDataError ? (
+        <CustomSnackbar
+          open={isAccountDataError}
+          message="Error fetching Surveyors data."
+          severity="error"
+        />
+      ) : (
+        <>
+          <Grid container direction="column" rowSpacing={1}>
+            <Grid item xs={12}>
+              <Box display="flex" px={2} pt={3}>
+                <Typography variant="h2" component="h2">
+                  My Account
+                </Typography>
+              </Box>
+            </Grid>
+            <AccountDetail label="First Name" value={accountData.firstname} />
+            <AccountDetail label="Last Name" value={accountData.lastname} />
+            <AccountDetail label="Email" value={accountData.email} />
+            <AccountDetail label="Address" value={accountData.street_address} />
+            <AccountDetail label="Phone Number" value={accountData.phone} />
+          </Grid>
+          <Grid container direction="column" rowSpacing={4}>
+            <Grid item xs={12}>
+              <Box display="flex" mt={3} mb={2} px={2}>
+                <Button variant="contained" component={Link} to="edit">
+                  Edit
+                </Button>
+              </Box>
+              <Box p={1} sx={{ borderBottom: "1px dashed grey" }} />
+              <Box display="flex" mt={3} mb={2} px={2}>
+                <Button variant="contained" disabled={true}>
+                  Request New Assignment
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </>
+      )}
     </Box>
   );
 };
