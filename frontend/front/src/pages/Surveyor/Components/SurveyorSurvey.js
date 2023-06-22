6d3cd7cbe63039134364a8c0ed3f2f1c2827f877
Leo Westebbe <leo.westebbe@gmail.com>
2023-06-22 13:43:08 -0400

    add BackButton to SurveyorSurvey

--- frontend/front/src/pages/Surveyor/Components/SurveyorSurvey.js
+++ frontend/front/src/pages/Surveyor/Components/SurveyorSurvey.js
@@ -1,12 +1,16 @@
 import React, { forwardRef } from "react";
 import SurveyComponent from "../../../components/SurveyComponent/SurveyComponent";
+import { BackButton } from "./BackButton";
 
 export const SURVEYOR_SURVEY_ID = "1";
 export const SurveyorSurvey = forwardRef((props, ref) => (
-  <SurveyComponent
-    {...props}
-    surveyId={SURVEYOR_SURVEY_ID}
-    formSpacing={2}
-    ref={ref}
-  />
+  <>
+    <BackButton url="/surveyor/dashboard" description="dashboard" />
+    <SurveyComponent
+      {...props}
+      surveyId={SURVEYOR_SURVEY_ID}
+      formSpacing={2}
+      ref={ref}
+    />
+  </>
 ));
