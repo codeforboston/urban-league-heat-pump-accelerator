7ba06b433c3ae0f8a170c998534840c4f0adc8a1
Leo Westebbe <leo.westebbe@gmail.com>
2023-06-22 13:40:08 -0400

    create BreadcrumbNav component

---
+++ frontend/front/src/features/breadcrumb/BreadcrumbNav.js
@@ -0,0 +1,37 @@
+import { Breadcrumbs, Link, Paper, Stack } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  popBreadcrumb,
+  selectBreadcrumbs,
+  setBreadcrumbs,
+} from "./breadcrumbSlice";
+
+export const BreadcrumbNav = () => {
+  const dispatch = useDispatch();
+  const links = [...useSelector(selectBreadcrumbs)];
+ 
+  const handleClick = (url) => {
+    while (links.at(-1)?.url !== url && links.length) {
+      links.pop();
+    }
+    dispatch(setBreadcrumbs(links));
+  };
+
+  return (
+    <Stack justifyContent="center" alignItems="center" width="100%" mt="10px">
+      <Breadcrumbs separator=">">
+        {links.map((link, index) => (
+          <Link
+            component={RouterLink}
+            to={link.url}
+            sx={{ textTransform: "uppercase" }}
+            onClick={() => handleClick(link.url)}
+          >
+            {link?.description}
+          </Link>
+        ))}
+      </Breadcrumbs>
+    </Stack>
+  );
+};
