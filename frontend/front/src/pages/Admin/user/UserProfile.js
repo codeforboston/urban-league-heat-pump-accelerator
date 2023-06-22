1bbb403e4be71f910cdf8761b87239fba4280aab
Leo Westebbe <leo.westebbe@gmail.com>
2023-06-22 18:03:25 -0400

    clean up userProfile render logic and add AdminBackButton

--- frontend/front/src/pages/Admin/user/UserProfile.js
+++ frontend/front/src/pages/Admin/user/UserProfile.js
@@ -6,6 +6,7 @@ import { useForm, Controller } from "react-hook-form";
 import { useGetSurveyorQuery } from "../../../api/apiSlice";
 import Loader from "../../../components/Loader";
 import CustomSnackbar from "../../../components/CustomSnackbar";
+import { AdminBackButton } from "../../Surveyor/Components/AdminBackButton";
 
 const UserProfile = () => {
   const { uid } = useParams();
@@ -83,19 +84,6 @@ const UserProfile = () => {
     );
   }
 
-  if (isSurveyorDataLoading) {
-    return <Loader />;
-  }
-
-  if (isSurveyorError) {
-    return (
-      <CustomSnackbar
-        open={isSurveyorError}
-        message="Error fetching surveyor data."
-        severity="error"
-      />
-    );
-  }
   return (
     <Box
       display="flex"
@@ -103,147 +91,164 @@ const UserProfile = () => {
       alignItems="center"
       flexDirection="column"
     >
-      <ConfirmationModal
-        isOpen={deleteModal}
-        handleConfirm={() => confirmDelete()}
-        handleCancel={() => cancelDelete()}
-        confirmBtnText="Delete"
-        cancelBtnText="Cancel"
-        title="Confirm Delete"
-        message="Please confirm to delete this user."
-      />
-      <Box width={500} mt={5}>
-        <Box sx={{ bgcolor: "primary.main", color: "white" }} p={1}>
-          <Typography variant="h5">User Profile: {uid}</Typography>
-          {/* Could add user ID to header. */}
-          {/* Can't be edited, but could be helpful to display. */}
-        </Box>
-
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Controller
-            name={"firstName"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                disabled={!editMode}
-                onChange={onChange}
-                value={surveyorData?.firstname || value}
-                label="First Name"
-                variant="standard"
-                sx={{ width: "95%", mx: 2, mt: 3 }}
-              />
-            )}
+      <AdminBackButton description="users" url="/admin/user" />
+      {isSurveyorDataLoading ? (
+        <Loader />
+      ) : isSurveyorError ? (
+        <CustomSnackbar
+          open={isSurveyorError}
+          message="Error fetching surveyor data."
+          severity="error"
+        />
+      ) : (
+        <>
+          <ConfirmationModal
+            isOpen={deleteModal}
+            handleConfirm={() => confirmDelete()}
+            handleCancel={() => cancelDelete()}
+            confirmBtnText="Delete"
+            cancelBtnText="Cancel"
+            title="Confirm Delete"
+            message="Please confirm to delete this user."
           />
-          <Controller
-            name={"lastName"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                disabled={!editMode}
-                onChange={onChange}
-                value={surveyorData?.lastname || value}
-                label="Last Name"
-                variant="standard"
-                sx={{ width: "95%", mx: 2, mt: 3 }}
-              />
-            )}
-          />
-          <Controller
-            name={"email"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                disabled={!editMode}
-                onChange={onChange}
-                value={surveyorData?.email || value}
-                label="Email"
-                variant="standard"
-                sx={{ width: "95%", mx: 2, mt: 3 }}
-              />
-            )}
-          />
-          <Controller
-            name={"phone"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                disabled={!editMode}
-                onChange={onChange}
-                value={surveyorData?.phone || value}
-                label="Phone"
-                variant="standard"
-                sx={{ width: "95%", mx: 2, mt: 3 }}
-              />
-            )}
-          />
-          <Controller
-            name={"streetAddress"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                disabled={!editMode}
-                onChange={onChange}
-                value={surveyorData?.street_address || value}
-                label="Street Address"
-                variant="standard"
-                sx={{ width: "95%", mx: 2, mt: 3 }}
-              />
-            )}
-          />
-          <Controller
-            name={"city"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                disabled={!editMode}
-                onChange={onChange}
-                value={surveyorData?.city || value}
-                label="City"
-                variant="standard"
-                sx={{ width: "95%", mx: 2, mt: 3 }}
-              />
-            )}
-          />
-          <Controller
-            name={"zipCode"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                disabled={!editMode}
-                onChange={onChange}
-                value={surveyorData?.zipcode || value}
-                label="Zip Code"
-                variant="standard"
-                sx={{ width: "95%", mx: 2, mt: 3 }}
-              />
-            )}
-          />
-          <Controller
-            name={"state"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                disabled={!editMode}
-                onChange={onChange}
-                value={surveyorData?.state || value}
-                label="State"
-                variant="standard"
-                sx={{ width: "95%", mx: 2, mt: 3 }}
-              />
-            )}
-          />
-
-          {/* BUTTONS */}
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-            <Box pt={5} textAlign="left">
-              <Button variant="contained" sx={{ ml: 2 }} disabled={editMode}>
-                CHANGE PASSWORD
-              </Button>
+          <Box width={500} mt={5}>
+            <Box sx={{ bgcolor: "primary.main", color: "white" }} p={1}>
+              <Typography variant="h5">User Profile: {uid}</Typography>
+              {/* Could add user ID to header. */}
+              {/* Can't be edited, but could be helpful to display. */}
             </Box>
-            {formControlButtons}
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Controller
+                name={"firstName"}
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextField
+                    disabled={!editMode}
+                    onChange={onChange}
+                    value={surveyorData?.firstname || value}
+                    label="First Name"
+                    variant="standard"
+                    sx={{ width: "95%", mx: 2, mt: 3 }}
+                  />
+                )}
+              />
+              <Controller
+                name={"lastName"}
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextField
+                    disabled={!editMode}
+                    onChange={onChange}
+                    value={surveyorData?.lastname || value}
+                    label="Last Name"
+                    variant="standard"
+                    sx={{ width: "95%", mx: 2, mt: 3 }}
+                  />
+                )}
+              />
+              <Controller
+                name={"email"}
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextField
+                    disabled={!editMode}
+                    onChange={onChange}
+                    value={surveyorData?.email || value}
+                    label="Email"
+                    variant="standard"
+                    sx={{ width: "95%", mx: 2, mt: 3 }}
+                  />
+                )}
+              />
+              <Controller
+                name={"phone"}
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextField
+                    disabled={!editMode}
+                    onChange={onChange}
+                    value={surveyorData?.phone || value}
+                    label="Phone"
+                    variant="standard"
+                    sx={{ width: "95%", mx: 2, mt: 3 }}
+                  />
+                )}
+              />
+              <Controller
+                name={"streetAddress"}
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextField
+                    disabled={!editMode}
+                    onChange={onChange}
+                    value={surveyorData?.street_address || value}
+                    label="Street Address"
+                    variant="standard"
+                    sx={{ width: "95%", mx: 2, mt: 3 }}
+                  />
+                )}
+              />
+              <Controller
+                name={"city"}
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextField
+                    disabled={!editMode}
+                    onChange={onChange}
+                    value={surveyorData?.city || value}
+                    label="City"
+                    variant="standard"
+                    sx={{ width: "95%", mx: 2, mt: 3 }}
+                  />
+                )}
+              />
+              <Controller
+                name={"zipCode"}
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextField
+                    disabled={!editMode}
+                    onChange={onChange}
+                    value={surveyorData?.zipcode || value}
+                    label="Zip Code"
+                    variant="standard"
+                    sx={{ width: "95%", mx: 2, mt: 3 }}
+                  />
+                )}
+              />
+              <Controller
+                name={"state"}
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextField
+                    disabled={!editMode}
+                    onChange={onChange}
+                    value={surveyorData?.state || value}
+                    label="State"
+                    variant="standard"
+                    sx={{ width: "95%", mx: 2, mt: 3 }}
+                  />
+                )}
+              />
+
+              {/* BUTTONS */}
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box pt={5} textAlign="left">
+                  <Button
+                    variant="contained"
+                    sx={{ ml: 2 }}
+                    disabled={editMode}
+                  >
+                    CHANGE PASSWORD
+                  </Button>
+                </Box>
+                {formControlButtons}
+              </Box>
+            </form>
           </Box>
-        </form>
-      </Box>
+        </>
+      )}
     </Box>
   );
 };
