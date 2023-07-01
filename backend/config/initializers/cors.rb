8d3c2774427f5e4554df2b6115b3e1eefa4fba78
Matt DelSordo <mattdelsordo@gmail.com>
2023-07-01 17:31:18 -0400

    add delete to cors method whitelist
    resolves 417

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource  '*',
               headers: :any,
-              methods: %i[get post patch put options],
+              methods: %i[get post patch put options delete],
               expose: %w[access-token expiry token-type Authorization]
   end
 end
