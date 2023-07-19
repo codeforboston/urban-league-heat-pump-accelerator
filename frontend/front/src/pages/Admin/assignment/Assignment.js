d2aeadeca760ed74a36a56e831a704b37eb17c74
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-07-18 23:28:27 -0400

    removed the unassinged homes button inside of the Admin Assignments table page

--- frontend/front/src/pages/Admin/assignment/Assignment.js
+++ frontend/front/src/pages/Admin/assignment/Assignment.js
@@ -1,45 +1,11 @@
-import { Box, Button, Typography } from "@mui/material";
-
 import AssignTable from "./AssignTable";
 import ContainerTitle from "../component/ContainerTitle";
-import CustomSnackbar from "../../../components/CustomSnackbar";
-import Loader from "../../../components/Loader";
 import React from "react";
-import { useGetUnassignedIncompleteHomesQuery } from "../../../api/apiSlice";
-import { useGoToBreadcrumb } from "../../../hooks/useGoToBreadcrumb";
 
-const Assignment = () => {
-  const goToBreadcrumb = useGoToBreadcrumb();
-
-  const {
-    data: unassignedIncompleteHomesData,
-    isError: isUnassignedIncompleteHomesError,
-    isLoading: isUnassignedIncompleteHomesDataLoading,
-  } = useGetUnassignedIncompleteHomesQuery();
-
-  const handleUnassignedLink = () =>
-    goToBreadcrumb("unassigned", { url: "/admin/assignment/unassigned" });
-
-  return isUnassignedIncompleteHomesDataLoading ? (
-    <Loader />
-  ) : isUnassignedIncompleteHomesError ? (
-    <CustomSnackbar
-      open={isUnassignedIncompleteHomesError}
-      message="Error fetching unassigned homes data"
-      severity="error"
-    />
-  ) : (
-    <ContainerTitle name={"Assignments"}>
-      <Box my={2}>
-        <Button onClick={() => handleUnassignedLink()} variant={"outlined"}>
-          <Typography variant="h6">
-            Unassigned Homes: {unassignedIncompleteHomesData.length}
-          </Typography>
-        </Button>
-      </Box>
-      <AssignTable />
-    </ContainerTitle>
-  );
-};
+const Assignment = () => (
+  <ContainerTitle name={"Assignments"}>
+    <AssignTable />
+  </ContainerTitle>
+);
 
 export default Assignment;
