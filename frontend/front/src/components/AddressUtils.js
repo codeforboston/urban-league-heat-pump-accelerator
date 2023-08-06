6b19fb4648b677ff2b76fa47031d394c2855d26c
Matt DelSordo <mattdelsordo@gmail.com>
2023-08-06 14:31:25 -0400

    routing fixes

--- frontend/front/src/components/AddressUtils.js
+++ frontend/front/src/components/AddressUtils.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Card, CardContent } from "@mui/material";
 
 /**
  * Component for displaying a home address
  */
-export const AddressComponent = ({ home }) => (
-  <Card sx={{ margin: "1em" }}>
-    <CardContent>
-      <address>
-        <h3>{"Address:"}</h3>
-        <div>{`${home?.street_number} ${home?.street_name} #${home?.unit_number}`}</div>
-        <div>{`${home?.city}, ${home?.state} ${home?.zip_code}`}</div>
-      </address>
-    </CardContent>
-  </Card>
-);
+export const AddressComponent = ({ home }) => {
+  const line1 = useMemo(() => {
+    if (home) {
+      let line = `${home.street_number} ${home.street_name}`;
+      if (home.unit_number) {
+        line += ` #${home.unit_number}`;
+      }
+      return line;
+    }
+
+    return "";
+  }, [home]);
+
+  const line2 = useMemo(() => {
+    if (home) {
+      return `${home.city}, ${home.state} ${home.zip_code}`;
+    }
+    return "";
+  }, [home]);
+
+  return (
+    <Card sx={{ margin: "1em" }}>
+      <CardContent>
+        <address>
+          <h3>{"Address:"}</h3>
+          <div>{line1}</div>
+          <div>{line2}</div>
+        </address>
+      </CardContent>
+    </Card>
+  );
+};
 
 export const houseToString = (home) => {
   if (!home) {
