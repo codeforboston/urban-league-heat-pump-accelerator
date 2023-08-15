bec885d7370e7b5df948fc786fd1d4dad4bb1064
mcarre20 <marlycarre@gmail.com>
2023-08-15 19:06:10 -0400

    data submit

--- frontend/front/src/pages/Admin/home/CreateNewHome.js
+++ frontend/front/src/pages/Admin/home/CreateNewHome.js
@@ -1,25 +1,44 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Alert, Box, Button, TextField, Typography } from "@mui/material";
 import { Controller, useForm } from "react-hook-form";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useCreateHomeMutation } from "../../../api/apiSlice";
 
 const CreateNewHome = () => {
   const navigate = useNavigate();
-  const { handleSubmit, control } = useForm({
+  const { handleSubmit, control, reset } = useForm({
     defaultValues: {
       streetNumber: "",
-      address: "",
-      zipCode: "",
+      streetName: "",
+      unitNumber: "",
       city: "",
+      zipCode: "",
     },
   });
 
-  const onSubmit = (data) => console.log(data);
+  const [createHome, { isLoading, isSuccess, isError }] =
+    useCreateHomeMutation();
 
+  const onSubmit = async (data) => {
+    const homeData = {
+      streetNumber: data.streetNumber,
+      streetName: data.streetName,
+      unit_number: data.unitumber,
+      city: data.city,
+      zipCode: data.zipCode,
+    };
+    createHome(homeData);
+  };
   const handleCancel = () => {
     navigate("/admin/home");
   };
+
+  useEffect(() => {
+    if (isSuccess) {
+      reset();
+    }
+  }, [isSuccess, reset]);
 
   return (
     <Box
@@ -47,13 +66,39 @@ const CreateNewHome = () => {
             )}
           />
           <Controller
-            name={"address"}
+            name={"streetName"}
             control={control}
             render={({ field: { onChange, value } }) => (
               <TextField
                 onChange={onChange}
                 value={value}
-                label={"Address"}
+                label={"Street Name"}
+                variant="standard"
+                sx={{ width: "95%", mx: 2, mt: 3 }}
+              />
+            )}
+          />
+          <Controller
+            name={"unitNumber"}
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextField
+                onChange={onChange}
+                value={value}
+                label={"Unit Number"}
+                variant="standard"
+                sx={{ width: "95%", mx: 2, mt: 3 }}
+              />
+            )}
+          />
+          <Controller
+            name={"city"}
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextField
+                onChange={onChange}
+                value={value}
+                label={"City"}
                 variant="standard"
                 sx={{ width: "95%", mx: 2, mt: 3 }}
               />
@@ -72,28 +117,18 @@ const CreateNewHome = () => {
               />
             )}
           />
-
-          <Controller
-            name={"city"}
-            control={control}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                onChange={onChange}
-                value={value}
-                label={"City"}
-                variant="standard"
-                sx={{ width: "95%", mx: 2, mt: 3 }}
-              />
-            )}
-          />
-
+          {isError && (
+            <Alert severity="error" sx={{ my: 1 }}>
+              Error Saving home
+            </Alert>
+          )}
           <Box pt={5} textAlign="right">
             <Button
               variant="outlined"
               sx={{ ml: 2 }}
               onClick={handleSubmit(onSubmit)}
             >
-              Create
+              {isLoading ? "Creating" : "Create"}
             </Button>
             <Button
               variant="outlined"
