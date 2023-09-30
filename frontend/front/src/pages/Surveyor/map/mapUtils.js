6a5c927ba7d1267bd1823fb4b269953110c14636
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-09-30 16:45:16 -0400

    feature: If someone is using iOS (iPhone, iPod, iPad), it will open the apple app instead of google maps.
    1. Fixed a bug that one of the houses was getting cut off from the list of waypoints.
    2. Use ‘navigator.userAgent’ to know if the user is using iOS or not
    3. The link and waypoints are formatted differently for the two cases.
    4. On Apple Maps, the waypoints only work on drive mode. Therefor walk mode was removed. (It’s not supported either but if the user changes it to bike mode for example, they will lose the waypoints and think the waypoints don’t work at all.)

--- frontend/front/src/pages/Surveyor/map/mapUtils.js
+++ frontend/front/src/pages/Surveyor/map/mapUtils.js
@@ -1,11 +1,19 @@
 export const generateMapsLink = (locations) => {
-  const origin = locations[0];
   const destination = locations[locations.length - 1];
 
-  const waypoints = locations
-    .slice(1, -1)
-    .map((location) => `${location.latitude},${location.longitude}`)
-    .join("|");
+  if (navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
+    const waypoints = locations
+      .slice(0, -1)
+      .map((location) => `&daddr=${location.latitude},${location.longitude}`)
+      .join("");
 
-  return `https://www.google.com/maps/dir/?api=1&destination=${destination.latitude},${destination.longitude}&waypoints=${waypoints}&travelmode=walking`;
+    return `maps://?&daddr=${destination.latitude},${destination.longitude}${waypoints}`;
+  } else {
+    const waypoints = locations
+      .slice(0, -1)
+      .map((location) => `${location.latitude},${location.longitude}`)
+      .join("|");
+
+    return `https://www.google.com/maps/dir/?api=1&destination=${destination.latitude},${destination.longitude}&waypoints=${waypoints}&travelmode=walking`;
+  }
 };
