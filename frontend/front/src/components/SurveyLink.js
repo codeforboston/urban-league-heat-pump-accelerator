64c62376508325d9692f32bb5a6bc209ec2167a6
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-10-13 18:17:48 -0400

    feature: Add a dropdown button component for survey links.
       1.  Implement a button that will take the user to the survey, or display a dropdown if there is more then on survey
       2. Then the user can click on the survey that they want to go to.

---
+++ frontend/front/src/components/SurveyLink.js
@@ -0,0 +1,44 @@
+import { Button, Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const SurveyLink = ({ links, label = "VIEW", ...styles }) => {
+  const navigate = useNavigate();
+
+  // Dropdown for when there more then one servey
+  const [dropdownButtonEl, setDropdownButtonEl] = useState(null);
+  const openDropdown = (e) => setDropdownButtonEl(e.currentTarget);
+  const closeDropdown = () => setDropdownButtonEl(null);
+
+  const goToSurvey = (surveyId) => {
+    navigate(`/admin/survey/visit/${surveyId}`);
+  };
+
+  return (
+    <>
+      <Button
+        {...styles}
+        onClick={(e) => {
+          if (links.length === 1) goToSurvey(links[0]);
+          else openDropdown(e);
+        }}
+      >
+        {label}
+      </Button>
+      {/* Dropdown */}
+      <Menu
+        anchorEl={dropdownButtonEl}
+        open={!!dropdownButtonEl}
+        onClose={closeDropdown}
+      >
+        {links.map((link, index) => (
+          <MenuItem key={index} onClick={() => goToSurvey(link)}>
+            Survey {index + 1}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+};
+
+export default SurveyLink;
