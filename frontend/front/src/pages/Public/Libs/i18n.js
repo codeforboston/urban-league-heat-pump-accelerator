d56e99739fffeec3dbc4ec816d98e5b4f85c4b9f
thiagobadini <thiagobardini@hotmail.com>
2023-10-20 13:05:55 -0400

    Fixing the language detector - part 5

--- frontend/front/src/pages/Public/Libs/i18n.js
+++ frontend/front/src/pages/Public/Libs/i18n.js
@@ -1,34 +1,28 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-// import Backend from "i18next-http-backend";
 import enTranslations from "../locales/en.json";
 import ptTranslations from "../locales/pt-BR.json";
 import esTranslations from "../locales/es-419.json";
 import htTranslations from "../locales/ht.json";
 
+const userLangPref = localStorage.getItem("langPref") || "en-us";
+console.log("userLangPref", userLangPref);
+
 // configuration for i18next library
 // if (!i18next.isInitialized) {
 i18next
   .use(initReactI18next) // passes i18next down to react-i18next
-  // .use(Backend)
   .use(LanguageDetector)
   .init({
     debug: true, // change to true to debug i18next
-    fallbackLng: {
-      "pt-BR": ["pt"],
-      es: ["es"],
-      ht: ["ht"],
-      default: ["en"],
-    },
+    fallbackLng: ["en", "es"], // fallback language
     resources: {
       en: { translation: { ...enTranslations } }, // English - United States
       "pt-BR": { translation: { ...ptTranslations } }, // Portuguese - Brazil
       es: { translation: { ...esTranslations } }, // Spanish - Latin America
       ht: { translation: { ...htTranslations } }, // Haitian Creole - Haiti
     },
-    lng: "en-us", // default language
+    lng: userLangPref, // default language
   });
 // }
-
-// Add event listener here
