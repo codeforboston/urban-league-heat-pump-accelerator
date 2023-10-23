ae857e4f0a497f12d707ed4c75413c4fefbd8297
thiagobadini <thiagobardini@hotmail.com>
2023-10-23 13:17:57 -0400

    Translated the 'public/about-us' page using i18n

--- frontend/front/src/pages/Public/Pages/AboutUs/AboutUs.js
+++ frontend/front/src/pages/Public/Pages/AboutUs/AboutUs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation, Trans } from "react-i18next";
 import {
   Box,
   Typography,
@@ -18,6 +19,8 @@ import moc from "../../../../assets/images/partnersLogo/moc.png";
 import Heading1BlueBgGround from "../../Components/Typography/Heading1BlueBgGround";
 
 function AboutUs() {
+  const { t } = useTranslation();
+
   return (
     <Box
       mb={3}
@@ -28,18 +31,20 @@ function AboutUs() {
         minHeight: "calc(100vh - 520px)",
       }}
     >
-      <Heading1BlueBgGround text="About the Boston Heat Pump Accelerator" />
+      <Heading1BlueBgGround text={t("public.about-us.headingbg")} />
       <Container>
-        <Stack direction="column" justifyContent="center">
+        <Stack direction="column" justifyContent="center" mb={3}>
           <Box mb={{ xs: 2, md: 6 }}>
             <Typography variant="body">
-              <b>The Boston Heat Pump Accelerator (BHPA)</b> works to support
-              increased access for Boston homeowners to heat pumps and related
-              resources, helping to reduce costs for residents, increase
-              community resilience, and shift the region to low-carbon
-              technologies. This initiative takes no funding from companies or
-              heat pump installers - it supports overall access to heat pumps
-              only, via education and awareness efforts.
+              <Trans i18nKey="public.about-us.bhpa">
+                The <b>Boston Heat Pump Accelerator (BHPA)</b> works to support
+                increased access for Boston homeowners to heat pumps and related
+                resources, helping to reduce costs for residents, increase
+                community resilience, and shift the region to low-carbon
+                technologies. This initiative takes no funding from companies or
+                heat pump installers - it supports overall access to heat pumps
+                only, via education and awareness efforts.
+              </Trans>
             </Typography>
           </Box>
           <Box>
@@ -60,77 +65,43 @@ function AboutUs() {
               </Link>
               <Box>
                 <Typography variant="body">
-                  <Link
-                    href="https://www.ulem.org/"
-                    sx={{
-                      color: "inherit",
-                      textDecoration: "none",
-                    }}
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    <b>The Urban League of Eastern Massachusetts (ULEM)</b>
-                  </Link>{" "}
-                  is the sponsor of the BHPA effort. Since 1919, ULEM has
-                  delivered workforce and economic development services and
-                  programs to increase self-reliance of residents of the Boston
-                  community and surrounding metropolitan areas. ULEM is a 501c3
-                  nonprofit organization and one of the oldest affiliates within
-                  the National Urban League movement.{" "}
+                  <Trans i18nKey="public.about-us.ulem1">
+                    The
+                    <Link
+                      href="https://www.ulem.org/"
+                      className="about-us-partners"
+                      target="_blank"
+                      rel="noopener"
+                      style={{
+                        textDecoration: "none",
+                        color: "inherit",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Urban League of Eastern Massachusetts (ULEM)
+                    </Link>
+                    is the sponsor of the BHPA effort. Since 1919, ULEM has
+                    delivered workforce and economic development services and
+                    programs to increase self-reliance of residents of the
+                    Boston community and surrounding metropolitan areas. ULEM is
+                    a 501c3 nonprofit organization and one of the oldest
+                    affiliates within the National Urban League movement.
+                  </Trans>
                 </Typography>
 
                 <Typography variant="body">
-                  The BHPA project is directed by ULEM Board Member Christopher
-                  Scranton, in coordination with core partners, listed below.
+                  {t("public.about-us.ulem2")}
                 </Typography>
               </Box>
             </Stack>
           </Box>
 
-          <Box textAlign="center" mt={6}>
-            <Typography variant="title4">BHPA CORE PARTNERS</Typography>
+          <Box textAlign="center" mt={6} mb={3}>
+            <Typography variant="title4">
+              {t("public.about-us.bhpa-core-partners")}
+            </Typography>
           </Box>
-          <PartnerTile
-            partnerName="The Massachusetts Clean Energy Center (MassCEC)"
-            paragraphText="is a quasi-public state economic development agency dedicated to accelerating the growth of the clean energy sector across the Commonwealth, to spur job creation, deliver statewide environmental benefits, and to secure long-term economic growth for the people of Massachusetts. MassCEC provided primary funding to the BHPA project via the EmPower grant program."
-            image={mcec}
-            website="https://www.masscec.com/"
-          />
-          <Divider sx={{ width: "100%" }} />
-          <PartnerTile
-            partnerName="Code For Boston (CfB)"
-            paragraphText="is a volunteer civic technology and social change organization. CfB is made up of developers, designers, data geeks, citizen activists, and many others who use creative technology to solve civic and social problems. CfB staff and volunteers provided core elements of the BHPA project."
-            image={cfb}
-            website="https://www.codeforboston.org/"
-          />
-          <Divider sx={{ width: "100%" }} />
-          <PartnerTile
-            partnerName="Code For America (CfA)"
-            paragraphText="is a 501(c)(3) nonprofit founded in 2009 to improve government services for all, starting with those who need them most. The CfA Brigade Network provided in-kind staff support to the BHPA project via the 2022 Impact Sprints program."
-            image={cfa}
-            website="https://codeforamerica.org/"
-          />
-          <Divider sx={{ width: "100%" }} />
-          <PartnerTile
-            partnerName="The HeatSmart Alliance"
-            paragraphText="is a volunteer group that promotes adoption of low-emissions heating, cooling, and related technologies in local communities. The Alliance staff provided technical expertise and guidance to the BHPA project."
-            image={heatsmart}
-            website="https://heatsmartalliance.org/"
-          />
-          <Divider sx={{ width: "100%" }} />
-          <PartnerTile
-            partnerName="City of Boston - PowerCorpsBOS (PCB)"
-            paragraphText="is a green jobs program that provides young adults with training, career readiness support, and connections to employers in the green industry. PCB provided support to the BHPA surveying effort."
-            image={pcb}
-            website="https://www.boston.gov/departments/workforce-development/powercorpsbos"
-          />
-          <Divider sx={{ width: "100%" }} />
-          <PartnerTile
-            partnerName="The Mass Open Cloud (MOC)"
-            paragraphText="is a laboratory for cloud research and innovation. Since its creation in 2013, with support from the Mass Tech Collaborative, it has provided a production cloud that has enabled innovation by a broad community of industry and research partners, used by thousands of students and researchers, and supported tens of millions of dollars in associated research grants that have resulted in contributions to open source software and hundreds of publications."
-            image={moc}
-            website="https://www.boston.gov/departments/workforce-development/powercorpsbos"
-          />
+          <PartnerTile />
         </Stack>
       </Container>
     </Box>
