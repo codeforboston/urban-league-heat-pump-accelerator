e5d1e833be90bb1e53986f92dc72685a5130b50b
thiagobadini <thiagobardini@hotmail.com>
2023-10-25 15:55:34 -0400

    Replaced <a> tag with MuiLink from @mui/material for external links.

--- frontend/front/src/pages/Public/Components/Button/ButtonDarkBlue.js
+++ frontend/front/src/pages/Public/Components/Button/ButtonDarkBlue.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Button } from "@mui/material";
+import { Button, Link as MuiLink } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const ButtonDarkBklue = ({ text, to, children, externalLink }) => {
@@ -9,7 +9,7 @@ const ButtonDarkBklue = ({ text, to, children, externalLink }) => {
 
   if (externalLink) {
     return (
-      <a href={to} target="_blank" rel="noopener noreferrer">
+      <MuiLink href={to} target="_blank" rel="noopener noreferrer">
         <Button
           onClick={() => window.scrollTo(0, 0)}
           sx={{
@@ -28,7 +28,7 @@ const ButtonDarkBklue = ({ text, to, children, externalLink }) => {
         >
           {text ? text : children}
         </Button>
-      </a>
+      </MuiLink>
     );
   }
 
