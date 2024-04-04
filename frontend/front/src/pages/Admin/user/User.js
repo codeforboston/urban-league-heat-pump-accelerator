afe665621e6004c2d9e29793d6f3678ad70b169b
mcarre20 <62024804+mcarre20@users.noreply.github.com>
2024-04-03 20:15:17 -0400

    Update User.js

--- frontend/front/src/pages/Admin/user/User.js
+++ frontend/front/src/pages/Admin/user/User.js
@@ -1,6 +1,5 @@
 import { Box, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import { Box, Button } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import ContainerTitle from "../component/ContainerTitle";
