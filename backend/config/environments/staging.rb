2b8b910f0ad3af7b3e9f2a7b0362143a98877d63
Matthew Zagaja <mzagaja@users.noreply.github.com>
2024-05-15 00:36:17 +0000

    Update to satisfy ActionMailer.
    Co-authored-by: Andrew <andrewydev6@gmail.com>
    Co-authored-by: mcarre20 <mcarre20@users.noreply.github.com>
    Co-authored-by: Anna Westland-Tegart <aTegart@users.noreply.github.com>
    Co-authored-by: Mike Lynch <mikelynch@users.noreply.github.com>

--- backend/config/environments/staging.rb
+++ backend/config/environments/staging.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'bostonhpa.org' }
+  config.action_mailer.default_url_options = { host: 'staging.bostonhpa.org' }
 end
