dd7358e048a6b09ae8d14571972f5ac68ea32e2a
Andrew <andrewydev6@gmail.com>
2024-07-10 02:40:05 +0000

    increase readability styles to SurveyorSurvey

--- frontend/front/src/pages/Surveyor/Components/SurveyorSurvey.js
+++ frontend/front/src/pages/Surveyor/Components/SurveyorSurvey.js
@@ -3,14 +3,30 @@ import SurveyComponent from "../../../components/SurveyComponent/SurveyComponent
 import { BackButton } from "./BackButton";
 
 export const SURVEYOR_SURVEY_ID = "1";
-export const SurveyorSurvey = forwardRef((props, ref) => (
-  <>
-    <BackButton url="/surveyor/dashboard" description="dashboard" />
-    <SurveyComponent
-      {...props}
-      surveyId={SURVEYOR_SURVEY_ID}
-      formSpacing={2}
-      ref={ref}
-    />
-  </>
-));
+export const SurveyorSurvey = forwardRef((props, ref) => {
+  const styles = {
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      gap: "1rem",
+    },
+    label: {
+      fontSize: "1.25rem",
+      fontWeight: 700,
+      color: "#000000",
+      lineHeight: "1.6",
+    },
+  };
+  return (
+    <>
+      <BackButton url="/surveyor/dashboard" description="dashboard" />
+      <SurveyComponent
+        {...props}
+        surveyId={SURVEYOR_SURVEY_ID}
+        formSpacing={8}
+        ref={ref}
+        styles={styles}
+      />
+    </>
+  );
+});
