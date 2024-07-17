52ad6c94553389ea0b3b8ea55bb5692426aaf452
Anna Westland-Tegart <27782045+aTegart@users.noreply.github.com>
2024-07-17 01:24:22 +0000

    fix pundit auth error for registrations controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   include Pundit::Authorization
-  after_action :verify_authorized, except: :index
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index
 
   # this fixed user creation but I have no idea WHY
