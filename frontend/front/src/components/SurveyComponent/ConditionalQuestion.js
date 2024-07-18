0dc376e4ed0a6a1606779f0ba1f85041c00257ba
Andrew <andrewydev6@gmail.com>
2024-07-18 17:50:58 -0400

    reset conditional question's answer on unmount (#584)

--- frontend/front/src/components/SurveyComponent/ConditionalQuestion.js
+++ frontend/front/src/components/SurveyComponent/ConditionalQuestion.js
@@ -1,13 +1,29 @@
+import { useEffect } from "react";
 import { useWatch } from "react-hook-form";
 
-const ConditionalQuestion = ({ control, children, rule }) => {
+const ConditionalQuestion = ({
+  id,
+  control,
+  children,
+  rule,
+  setValue,
+  formDefault,
+}) => {
   const watchedValue = useWatch({
     control,
     name: rule ? `${rule.question}` : "",
   });
+
+  useEffect(() => {
+    if (rule && watchedValue !== rule.answer) {
+      setValue(id.toString(), formDefault[id]);
+    }
+  }, [watchedValue, rule, id, setValue, formDefault]);
+
   if (rule && watchedValue !== rule.answer) {
     return null;
   }
+
   return <>{children}</>;
 };
 
