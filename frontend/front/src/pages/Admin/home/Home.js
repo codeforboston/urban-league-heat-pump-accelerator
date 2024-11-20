fc51d13974f0babd53b9aea712bfec156a8b337a
Andrew <andrewydev6@gmail.com>
2024-11-19 19:38:50 -0500

    Remove create home route (#647)

--- frontend/front/src/pages/Admin/home/Home.js
+++ frontend/front/src/pages/Admin/home/Home.js
@@ -1,25 +1,9 @@
-import { Box, Button } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import { Link } from "react-router-dom";
 import ContainerTitle from "../component/ContainerTitle";
 import HomeTable from "./HomeTable";
 
 const Home = () => {
   return (
     <ContainerTitle name="HOMES">
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Box m={2}>
-          <Button
-            variant="contained"
-            component={Link}
-            startIcon={<AddIcon />}
-            to="createHome"
-          >
-            Create New Home
-          </Button>
-        </Box>
-      </Box>
-
       <HomeTable />
     </ContainerTitle>
   );
