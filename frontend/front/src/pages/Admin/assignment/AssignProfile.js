350d22d7feb689c0ea60a41bc5160afa9b8d69ec
Andrew <andrewydev6@gmail.com>
2024-11-23 03:13:31 +0000

    Move assigments map component to assignment index route

--- frontend/front/src/pages/Admin/assignment/AssignProfile.js
+++ frontend/front/src/pages/Admin/assignment/AssignProfile.js
@@ -7,7 +7,6 @@ import {
   useGoToBreadcrumb,
   useInitBreadcrumbs,
 } from "../../../hooks/breadcrumbHooks";
-import { AssignmentsMap } from "../component/AssignmentsMap";
 import ContainerTitle from "../component/ContainerTitle";
 import CustomSnackbar from "../../../components/CustomSnackbar";
 import { DataGrid } from "@mui/x-data-grid";
@@ -134,7 +133,6 @@ const AssignProfile = () => {
         />
       ) : (
         <Box>
-          <AssignmentsMap />
           <Stack
             py={3}
             justifyContent="flex-start"
--- frontend/front/src/pages/Admin/assignment/Assignment.js
+++ frontend/front/src/pages/Admin/assignment/Assignment.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { AssignmentsMap } from "../component/AssignmentsMap";
 import AssignTable from "./AssignTable";
 import ContainerTitle from "../component/ContainerTitle";
 
 const Assignment = () => (
   <ContainerTitle name={"Assignments"}>
+    <AssignmentsMap />
     <AssignTable />
   </ContainerTitle>
 );
