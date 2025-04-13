e54501a3c5130ccafc016852b0696fa4e1937d51
Andrew <andrewydev6@gmail.com>
2025-04-13 04:12:00 +0000

    setup newrelic browser agent

---
+++ frontend/front/src/features/newrelic/index.js
@@ -0,0 +1,19 @@
+import { BrowserAgent } from "@newrelic/browser-agent/loaders/browser-agent";
+
+export const isNewRelicEnabled = () => {
+  return (
+    process.env.REACT_APP_NEW_RELIC_INIT &&
+    process.env.REACT_APP_NEW_RELIC_INFO &&
+    process.env.REACT_APP_NEW_RELIC_LOADER_CONFIG
+  );
+};
+
+let newrelic = null;
+if (isNewRelicEnabled()) {
+  const options = {
+    init: JSON.parse(process.env.REACT_APP_NEW_RELIC_INIT),
+    info: JSON.parse(process.env.REACT_APP_NEW_RELIC_INFO),
+    loader_config: JSON.parse(process.env.REACT_APP_NEW_RELIC_LOADER_CONFIG),
+  };
+  newrelic = new BrowserAgent(options);
+}
