1322f772551f10622f34b2fc0e128ef625ee8dd2
Laura Filmeyer <filmeyer.dev@gmail.com>
2025-05-07 15:57:47 -0400

    Validate value of language code on survey response (#691)

--- backend/app/models/survey_response.rb
+++ backend/app/models/survey_response.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class SurveyResponse < ApplicationRecord
+  SUPPORTED_LANGUAGE_CODES = %w[en es ht pt].freeze
+
   belongs_to :survey
   belongs_to :survey_visit
   has_many :survey_answers, dependent: :destroy
   accepts_nested_attributes_for :survey_answers
+
+  validates :language_code, inclusion: { in: SUPPORTED_LANGUAGE_CODES }, allow_nil: true
 end
--- backend/spec/models/survey_response_spec.rb
+++ backend/spec/models/survey_response_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe SurveyResponse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates language code is an accepted value if not null' do
+    survey_response = build(:survey_response, language_code: 'foo')
+    survey_response.valid?
+    expect(survey_response.errors.full_messages).to include('Language code is not included in the list')
+  end
 end
