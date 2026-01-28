6827907bfe31ef781b95af5bfb707bff60e8060b
Andrew <andrewydev6@gmail.com>
2026-01-28 02:07:54 +0000

    fix public survey question color mismatch
    Co-authored-by: Anna Westland-Tegart <aTegart@users.noreply.github.com>
    Co-authored-by: Marly <mcarre20@users.noreply.github.com>
    Co-authored-by: Matthew Zagaja <mzagaja@alum.wpi.edu>

--- frontend/front/src/pages/Public/Components/PublicSurvey.js
+++ frontend/front/src/pages/Public/Components/PublicSurvey.js
@@ -8,12 +8,20 @@ export const PublicSurvey = forwardRef((props, ref) => {
   const {
     i18n: { language },
   } = useTranslation();
+
+  const styles = {
+    label: {
+      color: "#000000",
+    },
+  };
+
   return (
     <SurveyComponent
       {...props}
       surveyId={PUBLIC_SURVEY_ID}
       formSpacing={5}
       ref={ref}
+      styles={styles}
       langPref={language}
       surveyMode="online"
     />
